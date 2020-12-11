f00ac59a11064d5f01ce383c6f0802a675854c43
jason Hilton <jahilton@stanford.edu>
2020-12-11 12:06:36 -0800

    add comments

--- scripts/get_dataset.py
+++ scripts/get_dataset.py
@@ -36,10 +36,12 @@ def get_object_and_links(obj_id):
     url = urljoin(server, obj_id + '/?format=json&frame=object')
     temp_obj = requests.get(url, auth=connection.auth).json()
 
+    # remove fields with empty values or unnecessary fields
     my_obj = {k: v for k, v in temp_obj.items() if v and k not in ignore_props}
 
     obj_type = my_obj['@type'][0]
 
+    # add the object if we haven't already done so
     if whole_dict.get(obj_type) and my_obj.get('uuid') not in added_uuids:
         whole_dict[obj_type].append(my_obj)
         added_uuids.add(my_obj['uuid'])
@@ -47,6 +49,7 @@ def get_object_and_links(obj_id):
         whole_dict[obj_type] = [my_obj]
         added_uuids.add(my_obj['uuid'])
 
+    # get identifiers for any object that referenced by this one
     for prop in my_obj.keys():
         if schemas[obj_type]['properties'][prop].get('linkTo'):
             next_remaining.add(my_obj[prop])
@@ -57,11 +60,10 @@ def get_object_and_links(obj_id):
 ignore_props = [
     'original_files', # these are all captured in files
     'fileset', # we don't need to grab the ReferenceFileSet objects
-    'submitted_by'
+    'submitted_by' # our internal users aren't relevant
 ]
 
 args = getArgs()
-
 connection = lattice.Connection(args.mode)
 server = connection.server
 
@@ -71,7 +73,7 @@ schemas = requests.get(schema_url).json()
 dataset_id = args.dataset
 
 whole_dict = {}
-added_uuids = set()
+added_uuids = set() # we want to keep track to avoid duplicating objects
 seen = set()
 remaining = set([dataset_id])
 
@@ -82,9 +84,10 @@ while remaining:
         get_object_and_links(identifier)
     remaining = next_remaining - seen
 
-#make directory named after dataset
+# make directory named after dataset
 os.mkdir(dataset_id)
 
+# write json files for each object type in the dataset directory
 for k in whole_dict.keys():
     with open(dataset_id + '/' + k + '.json', 'w') as outfile:
         json.dump(whole_dict[k], outfile, indent=4)
