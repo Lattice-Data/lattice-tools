30836e19422107ea9911bd0e5be4e398bcaaa739
Jenny Chien <jenny@Jennys-MacBook-Pro.local>
2021-04-26 18:30:41 -0700

    read gtf only once in calc_gene

--- scripts/h5ad_conversion.py
+++ scripts/h5ad_conversion.py
@@ -40,7 +40,7 @@ def getArgs():
 	return args
 
 
-def add_gene(adata, args):
+def calc_gene(args):
 	df_gtf = pd.DataFrame()
 	ensembl = ""
 	symbol = ""
@@ -53,15 +53,24 @@ def add_gene(adata, args):
 				ensembl = re.search(r'(ENSG\d+)', gene_info[0])[1]
 				symbol = re.search(r'"(.*)"', gene_info[2])[1]
 				df_gtf = df_gtf.append(pd.DataFrame({'symbol': symbol, 'feature_types': args.feature_type, 'genome': args.genome_version}, index=[ensembl]))
-	adata.var = pd.merge(adata.var, df_gtf, left_index=True, right_index=True)
+	return df_gtf
+
+
+def add_gene(adata, args, df_gtf):
+	adata.var = pd.merge(adata.var, df_gtf, left_index=True, right_index=True, how='left')
 	adata.var['gene_ids'] = adata.var.index
 	adata.var = adata.var.set_index('symbol', drop=True)
+	col_order = adata.var.columns.tolist()
+	col_order = col_order[-1:] + col_order[:-1]
+	adata.var = adata.var[col_order]
 	adata.var.index.name = None
 
 
 def main(args):
 	all_files = os.listdir(args.indir)
 	os.mkdir(temp_dir)
+	if args.gtf is not None:
+		df_gtf = calc_gene(args)
 	for filename in all_files:
 		if filename.endswith('gz'):
 			with gzip.open(os.path.join(args.indir, filename), mode='rb') as f_in:
@@ -74,7 +83,7 @@ def main(args):
 		if args.transpose:
 			adata = adata.transpose()
 		if args.gtf is not None:
-			add_gene(adata, args)
+			add_gene(adata, args, df_gtf)
 		adata.write(os.path.join(args.outdir, filename.replace('.txt.gz', '.h5ad')))
 
 	shutil.rmtree(temp_dir)
