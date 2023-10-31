8d59d784b0f0fe14e95d229bcbd7e33e2ebd513c
Jim Chaffer <77802611+Jchaffer787@users.noreply.github.com>
2023-10-31 15:36:15 -0700

    Add check for right obj type and fix error
    Added a check that the obj is the right type to have 'primary_portion', as well as fixed an error due to having a series returned instead of a list.

--- scripts/flattener.py
+++ scripts/flattener.py
@@ -1421,16 +1421,17 @@ def main(mfinal_id):
 		cxg_obs = pd.merge(cxg_obs, df[['disease_ontology_term_id', 'reported_diseases', 'sex_ontology_term_id']], left_on="raw_matrix_accession", right_index=True, how="left" )
 
 	# Check that primary_portion.obs_field of ProcessedMatrixFile is present in cxg_obs
-	primary_portion = mfinal_obj.get('primary_portion')
-	if primary_portion.get('obs_field') not in cxg_obs.columns:
-		logging.error("ERROR: 'obs_field' value '{}' not found in cxg_obs columns".format(primary_portion.get('obs_field')))
-		sys.exit("ERROR: 'obs_field' value '{}' not found in cxg_obs columns".format(primary_portion.get('obs_field')))
+	if mfinal_obj['is_primary_data'] == 'mixed':
+		primary_portion = mfinal_obj.get('primary_portion')
+		if primary_portion.get('obs_field') not in cxg_obs.columns:
+			logging.error("ERROR: 'obs_field' value '{}' not found in cxg_obs columns".format(primary_portion.get('obs_field')))
+			sys.exit("ERROR: 'obs_field' value '{}' not found in cxg_obs columns".format(primary_portion.get('obs_field')))
 
-	# Check that all primary_portion.values of ProcessedMatrixFile are found in the 'obs_field' column of cxg_obs
-	missing = [f for f in primary_portion.get('values') if f not in cxg_obs[primary_portion.get('obs_field')]]
-	if missing:
-		logging.error("ERROR: cxg_obs column '{}' doesn't contain values present in 'primary_portion.obs_field' of ProcessedMatrixFile: {}".format(primary_portion.get('obs_field'),missing))
-		sys.exit("ERROR: cxg_obs column '{}' doesn't contain values present in 'primary_portion.obs_field' of ProcessedMatrixFile: {}".format(primary_portion.get('obs_field'),missing))
+		# Check that all primary_portion.values of ProcessedMatrixFile are found in the 'obs_field' column of cxg_obs
+		missing = [f for f in primary_portion.get('values') if f not in cxg_obs[primary_portion.get('obs_field')].tolist()]
+		if missing:
+			logging.error("ERROR: cxg_obs column '{}' doesn't contain values present in 'primary_portion.obs_field' of ProcessedMatrixFile: {}".format(primary_portion.get('obs_field'),missing))
+			sys.exit("ERROR: cxg_obs column '{}' doesn't contain values present in 'primary_portion.obs_field' of ProcessedMatrixFile: {}".format(primary_portion.get('obs_field'),missing))
 
 	# Clean up columns in obs to follow cxg schema and drop any unnecessary fields
 	drop_cols(celltype_col)
