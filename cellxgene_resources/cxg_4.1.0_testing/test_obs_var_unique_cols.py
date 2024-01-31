24a156659cdbc98cb88880b117f9cc92189fa819
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-01-31 12:53:36 -0800

    added obs and var unique name tests

---
+++ cellxgene_resources/cxg_4.1.0_testing/test_obs_var_unique_cols.py
@@ -0,0 +1,118 @@
+"""
+QA testing for this issue:
+https://github.com/chanzuckerberg/single-cell-curation/issues/729
+
+Ways pandas DF can allow duplicate column names:
+    concat same DF onto itself with second argument containing name of col that already exists
+    two or more cols are renamed to the same string
+"""
+
+import os
+import sys
+import anndata as ad
+import pandas as pd
+import pytest
+
+scc_repo_loc = os.path.expanduser('~/GitClones/CZI/')
+
+sys.path.append(os.path.abspath(scc_repo_loc + 'single-cell-curation/cellxgene_schemea_cli/'))
+
+from cellxgene_schema.validate import Validator
+
+
+@pytest.fixture
+def validator_with_adata() -> Validator:
+    validator = Validator()
+    validator.adata = ad.read_h5ad('fixtures/valid.h5ad')
+    return validator
+
+
+@pytest.mark.parametrize(
+    "duplicate_col",
+    (
+        pytest.param('suspension_type', id="duplicated obs col 'suspension_type'"),
+        pytest.param('donor_id', id="duplicated obs col 'donor_id'"),
+        pytest.param('author_test', id="duplicated obs author col 'author_test'"),
+    )
+)
+def test_obs_concat_unique_fails(validator_with_adata, duplicate_col):
+    validator = validator_with_adata
+    validator.adata.obs['author_test'] = 'test'
+    validator.adata.obs = pd.concat([validator.adata.obs, validator.adata.obs[duplicate_col]], axis=1)
+
+    with pytest.raises(ValueError):
+        validator.validate_adata()
+
+
+@pytest.mark.parametrize(
+    "duplicate_col",
+    (
+        pytest.param('feature_is_filtered', id="duplicated var col 'feature_is_filtered'"),
+    )
+)
+def test_var_concat_unique_fails(validator_with_adata, duplicate_col):
+    validator = validator_with_adata
+    validator.adata.var = pd.concat([validator.adata.var, validator.adata.var[duplicate_col]], axis=1)
+
+    with pytest.raises(ValueError):
+        validator.validate_adata()
+
+
+@pytest.mark.parametrize(
+    "col_rename_dict",
+    (
+        pytest.param({'cluster_color': 'cluster', 'size': 'cluster'}, id="rename author obs col"),
+        pytest.param({'cluster_color': 'suspension_type'}, id="rename to required obs col"),
+    )
+)
+def test_obs_rename_unique_fails(validator_with_adata, col_rename_dict):
+    validator = validator_with_adata
+    validator.adata.obs.rename(columns=col_rename_dict, inplace=True)
+
+    with pytest.raises(ValueError):
+        validator.validate_adata()
+
+        
+@pytest.mark.parametrize(
+    "col_rename_dict",
+    (
+        pytest.param({'test': 'feature_is_filtered'}, id="rename var col to required name"),
+        pytest.param({'test': 'gene_symbol'}, id="rename var col to author name"),
+    )
+)
+def test_var_rename_unique_fails(validator_with_adata, col_rename_dict):
+    validator = validator_with_adata
+    validator.adata.var['test'] = 'test'
+    validator.adata.var['gene_symbol'] = 'test'
+    validator.adata.var.rename(columns=col_rename_dict, inplace=True)
+
+    with pytest.raises(ValueError):
+        validator.validate_adata()
+
+
+# testing for raw.var duplicates, more complicated pathway we take to merge in raw
+# matrix and raw.var
+@pytest.mark.parametrize(
+    "duplicate_col",
+    (
+        pytest.param('gene_symbol', id="duplicated raw var 'gene_symbol'"),
+        pytest.param('ensembl_version', id="duplicated raw var 'ensembl_version'"),
+    )
+)
+def test_raw_var_concat_fails(duplicate_col):
+    adata = ad.read_h5ad('fixtures/valid.h5ad')
+    var = adata.raw.var
+    var[duplicate_col] = 'test'
+    new_var = pd.concat([var, var[duplicate_col]], axis=1)
+    raw_adata = ad.AnnData(adata.raw.X, obs=adata.obs, var=new_var)
+    adata.raw = raw_adata
+    
+    # quick check to make sure only raw.var contains duplicates
+    print(adata.var)
+    print(adata.raw.var)
+
+    validator = Validator()
+    validator.adata = adata
+
+    with pytest.raises(ValueError):
+        validator.validate_adata()
