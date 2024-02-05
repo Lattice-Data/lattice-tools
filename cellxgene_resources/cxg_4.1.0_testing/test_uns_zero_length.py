eb85fd99fba3f9bdf1a63d139dedfed69cb4247e
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-02-05 09:46:17 -0800

    fixed uns zero-length tests

--- cellxgene_resources/cxg_4.1.0_testing/test_uns_zero_length.py
+++ cellxgene_resources/cxg_4.1.0_testing/test_uns_zero_length.py
@@ -1,6 +1,7 @@
 """
 QA testing for this issue:
 https://github.com/chanzuckerberg/single-cell-curation/issues/728
+https://github.com/chanzuckerberg/single-cell-curation/pull/753
 """
 
 import os
@@ -29,6 +30,9 @@ def validator_with_adata() -> Validator:
         pytest.param('title', 'Here is my title', True, id='Normal string title'),
         pytest.param('batch_condition', ['donor_id'], True, id='Normal batch_condition list'),
         pytest.param('log1p', np.array([1, 2, 3, 4]), True, id='Normal author np.array'),
+        pytest.param('log1p', True, True, id='Value set to True'),
+        pytest.param('log1p', False, True, id='Value set to False'),
+        pytest.param('log1p', None, True, id='Value set to None'),
     )
 )
 def test_uns_zero_length_passes(validator_with_adata, uns_key, empty_value, expected):
@@ -49,9 +53,6 @@ def test_uns_zero_length_passes(validator_with_adata, uns_key, empty_value, expe
         pytest.param('log1p', "", False, id='Empty string, author key'),
         pytest.param('log1p', {}, False, id='Empty dictionary, author key'),
         pytest.param('log1p', (), False, id='Empty set, author key'),
-        pytest.param('log1p', True, False, id='Value set to True'),
-        pytest.param('log1p', False, False, id='Value set to False'),
-        pytest.param('log1p', None, False, id='Value set to None'),
     )
 )
 def test_uns_zero_length_fails(validator_with_adata, uns_key, empty_value, expected):
@@ -76,3 +77,21 @@ def test_uns_zero_length_fails_many_errors(validator_with_adata, uns_key, empty_
     validator.validate_adata()
     assert validator.is_valid is expected
     assert f"ERROR: uns['{uns_key}'] cannot be an empty value." in validator.errors 
+
+
+@pytest.mark.parametrize(
+    "bool_value,expected",
+    (
+        pytest.param(True, False, id='title set to True'),
+        pytest.param(False, False, id='title set to False'),
+        pytest.param(None, False, id='title set to None'),
+    )
+)
+def test_uns_title_bool_none_fails(validator_with_adata, bool_value, expected):
+    validator = validator_with_adata
+    validator.adata.uns['title'] = bool_value
+    validator.validate_adata()
+    assert validator.is_valid is expected
+    assert validator.errors == [
+        f"ERROR: '{bool_value}' in 'uns['title']' is not valid, it must be a string."
+    ]
