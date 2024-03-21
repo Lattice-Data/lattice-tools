71d7ea61e2db89603d191cc6e6d5d8876ccf46f7
jahilton <jahilton@stanford.edu>
2024-03-21 12:57:33 -0700

    add comments

--- gene_ID_mapping/gene_mapping.py
+++ gene_ID_mapping/gene_mapping.py
@@ -58,6 +58,7 @@ def create_df(file, hg19=False):
     column_names = ['seqname','source','feature','start','stop','score','strand','frame','expand_me']
     gtf_df = pd.read_table(file, names=column_names, comment='#')
 
+    #parse out transcripts
     transcript_df = gtf_df[gtf_df['feature'] == 'transcript']
     transcript_df = transcript_df['expand_me'].str.split(';',expand=True)
     for c in transcript_df.columns:
@@ -68,12 +69,14 @@ def create_df(file, hg19=False):
     transcript_df['gene_id'] = transcript_df['gene_id'].apply(lambda x: x.split('"')[1].split('.')[0])
     transcript_df['transcript_id'] = transcript_df['transcript_id'].apply(lambda x: x.split('"')[1].split('.')[0])
     transcript_df = transcript_df.groupby('gene_id').agg(lambda x: ','.join(sorted(set(x)))).reset_index()
-    
+
+    #parse out genes
     gene_df = gtf_df[gtf_df['feature'] == 'gene']
     gene_df[['gene_id','gene_name']] = gene_df['expand_me'].str.split(';',expand=True)[[0,2]]
     gene_df['gene_name'] = gene_df['gene_name'].apply(lambda x: x.split('"')[1])
     gene_df['gene_id'] = gene_df['gene_id'].apply(lambda x: x.split('"')[1].split('.')[0])
 
+    #merge transcript lists to the gene table
     gene_df = gene_df.merge(transcript_df, on='gene_id', how='left')
 
     if hg19:
@@ -146,6 +149,7 @@ schema_df = create_df(schema_ref)
 all_inputs = pd.DataFrame()
 ref_comp = pd.DataFrame()
 
+#compare each version to the target version
 for r in refs:
     print(r)
     if 'v19' in r:
@@ -168,10 +172,11 @@ min_report['input'] = min_report['input'].apply(
     lambda x: x.replace('gencode.','').replace('.annotation.gtf.gz','')
         .replace('.chr_patch_hapl_scaff','').replace('.primary_assembly','')
     )
-
+#remove any where two IDs map to the same target ID in the same old version
 invalid_mappings = min_report[min_report.duplicated(subset='new_ENSG-input',keep=False)]['gene_id-new_ENSG'].unique()
 min_report = min_report[min_report['gene_id-new_ENSG'].isin(invalid_mappings) == False]
 
+#remove any where two IDs map to the same target ID amongst all old versions
 invalid_new_ENSG = []
 all_new = [i.split('-')[-1] for i in min_report['gene_id-new_ENSG'].unique()]
 dup_new = set([i for i in all_new if all_new.count(i) > 1])
@@ -216,7 +221,7 @@ min_report = min_report[['gene_id','mapped in','new_ENSG']]
 
 min_report = min_report.merge(schema_df[['gene_id','gene_name']],
                     how='left', left_on='new_ENSG', right_on='gene_id',
-                    suffixes=(None,'_y'))#.drop(columns=['gene_id_y'])
+                    suffixes=(None,'_y'))
 min_report = min_report.merge(min_all_inputs, how='left', on='gene_id')
 min_report.fillna('', inplace=True)
 
