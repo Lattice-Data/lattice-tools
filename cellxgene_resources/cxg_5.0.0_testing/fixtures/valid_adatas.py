e7ba1388763f87254f2e9d8b66bfdad0b31b4a17
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-04-02 14:02:18 -0700

    working tests for schema version and reference

--- cellxgene_resources/cxg_5.0.0_testing/fixtures/valid_adatas.py
+++ cellxgene_resources/cxg_5.0.0_testing/fixtures/valid_adatas.py
@@ -11,6 +11,7 @@ SCC_REPO_LOC = os.path.expanduser('~/GitClones/CZI/')
 sys.path.append(os.path.abspath(SCC_REPO_LOC + 'single-cell-curation/cellxgene_schemea_cli/'))
 
 from cellxgene_schema.validate import Validator
+from cellxgene_schema.write_labels import AnnDataLabelAppender
 
 
 # fixture exported to other tests, returns and therefor tests with each h5ad
@@ -19,3 +20,10 @@ def validator_with_adata(request, fixtures_root=FIXTURES_ROOT) -> Validator:
     validator = Validator()
     validator.adata = ad.read_h5ad(f'{fixtures_root}/{request.param}')
     return validator
+
+
+@pytest.fixture
+def label_writer(validator_with_adata):
+    validator = validator_with_adata
+    validator.validate_adata()
+    return AnnDataLabelAppender(validator)
