35e8a61eea557bad4378dac5ca23be8976b96105
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-04-11 11:09:28 -0700

    boto3 upload_obj function

--- scripts/compress_s3_files.py
+++ scripts/compress_s3_files.py
@@ -134,29 +134,6 @@ def s3_exists(uri_info: URIMetaInfo) -> bool:
         return False
 
 
-def upload_file(file_name, bucket, object_name=None) -> bool:
-    """
-    Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    try:
-        response = S3_CLIENT.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-
 def main(s3_uri_file):
     if not local_exists([TEMP_DIR]):
         os.mkdir(TEMP_DIR)
@@ -184,9 +161,9 @@ def main(s3_uri_file):
             print(f"File {uri.new_file_name} already compressed in {TEMP_DIR}")
 
         # upload
-        print(f"Uploading curated h5ad to this object key: {uri.new_file_path}")
-        with open(TEMP_DIR + "/" + uri.new_file_name, "rb") as f:
-            upload_file(f, uri.bucket_name, uri.new_file_path)
+        print(f"Uploading compressed h5ad to this object key: {uri.file_path}")
+        with open(os.path.join(TEMP_DIR, uri.new_file_name), "rb") as f:
+            S3_CLIENT.upload_fileobj(f, uri.bucket_name, uri.file_path)
 
         # remove h5ads
         if local_exists([TEMP_DIR, uri.file_name]):
