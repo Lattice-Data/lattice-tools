e98444b7105b5f182f298a0acf2417076bc26871
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-05-02 10:26:54 -0700

    type tests for is_primary_data col

--- cellxgene_resources/cxg_5.1.0_testing/is_primary_data_test.py
+++ cellxgene_resources/cxg_5.1.0_testing/is_primary_data_test.py
@@ -43,6 +43,21 @@ def test_non_spatial_is_primary_false(validator_with_non_spatial_adata):
     assert validator.errors == []
 
 
+# already present checks, don't think we have a specific test for this yet
+@pytest.mark.parametrize(
+    "type", (None, 1, 1.0, "True", "False")
+)
+def test_is_primary_data_types(validator_with_all_adatas, type):
+    validator = validator_with_all_adatas
+    validator.adata.obs["is_primary_data"] = type
+    col = validator.adata.obs["is_primary_data"]
+    validator.validate_adata()
+    assert validator.is_valid is False
+    assert validator.errors == [
+        f"ERROR: Column 'is_primary_data' in dataframe 'obs' must be boolean, not '{col.dtype}'."
+    ]
+
+
 # could be other instances of data in corpus even though uns["spatial"]["is_single"] is True
 def test_spatial_is_primary_false(validator_with_spatial_adatas):
     validator = validator_with_spatial_adatas
