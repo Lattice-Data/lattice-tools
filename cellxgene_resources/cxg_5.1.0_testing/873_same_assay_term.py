4f8768e313ee39ade986e7a439a82f2a1b958a65
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-05-08 16:19:09 -0700

    more same assay tests

--- cellxgene_resources/cxg_5.1.0_testing/873_same_assay_term.py
+++ cellxgene_resources/cxg_5.1.0_testing/873_same_assay_term.py
@@ -24,15 +24,40 @@ def test_mixed_assays_spatial(validator_with_spatial_adatas):
     validator = validator_with_spatial_adatas
     validator.adata.obs["assay_ontology_term_id"] = validator.adata.obs["assay_ontology_term_id"].cat.add_categories("EFO:0009922")
     validator.adata.obs.loc[validator.adata.obs.index[1], "assay_ontology_term_id"] = "EFO:0009922"
+    validator.validate_adata()
     assert validator.adata.obs["assay_ontology_term_id"].nunique() == 2
     assert validator.is_valid is False
-    assert validator.errors == ["here is my error"]
+    assert validator.errors[0] == "ERROR: When obs['assay_ontology_term_id'] is either 'EFO:0010961' (Visium Spatial Gene Expression) or 'EFO:0030062' (Slide-seqV2), all observations must contain the same value."
+
+
+def test_mixed_assays_slide_seq_is_single_false(validator_with_slide_seq_adatas):
+    validator = validator_with_slide_seq_adatas
+    validator.adata.obs["assay_ontology_term_id"] = validator.adata.obs["assay_ontology_term_id"].cat.add_categories("EFO:0009922")
+    validator.adata.obs.loc[validator.adata.obs.index[1], "assay_ontology_term_id"] = "EFO:0009922"
+    validator.adata.uns["spatial"]["is_single"] = False
+    validator.validate_adata()
+    assert validator.adata.obs["assay_ontology_term_id"].nunique() == 2
+    assert validator.is_valid is False
+    assert validator.errors[0] == "ERROR: When obs['assay_ontology_term_id'] is either 'EFO:0010961' (Visium Spatial Gene Expression) or 'EFO:0030062' (Slide-seqV2), all observations must contain the same value."
+
+
+def test_mixed_assays_visium_is_single_false(validator_with_all_visiums):
+    validator = validator_with_all_visiums
+    validator.adata.obs["assay_ontology_term_id"] = validator.adata.obs["assay_ontology_term_id"].cat.add_categories("EFO:0009922")
+    validator.adata.obs.loc[validator.adata.obs.index[1], "assay_ontology_term_id"] = "EFO:0009922"
+    validator.adata.uns["spatial"]["is_single"] = False
+    validator.validate_adata()
+    assert validator.adata.obs["assay_ontology_term_id"].nunique() == 2
+    assert validator.is_valid is False
+    assert validator.errors[1] == "ERROR: When obs['assay_ontology_term_id'] is either 'EFO:0010961' (Visium Spatial Gene Expression) or 'EFO:0030062' (Slide-seqV2), all observations must contain the same value."
 
 
 def test_mixed_assays_non_spatial(validator_with_non_spatial_adata):
     validator = validator_with_non_spatial_adata
     validator.adata.obs["assay_ontology_term_id"] = validator.adata.obs["assay_ontology_term_id"].cat.add_categories("EFO:0010961")
     validator.adata.obs.loc[validator.adata.obs.index[1], "assay_ontology_term_id"] = "EFO:0010961"
+    validator.validate_adata()
     assert validator.adata.obs["assay_ontology_term_id"].nunique() >= 2
     assert validator.is_valid is False
-    assert validator.errors == ["error for non spatial"]
+    assert validator.errors[1] == "ERROR: When obs['assay_ontology_term_id'] is either 'EFO:0010961' (Visium Spatial Gene Expression) or 'EFO:0030062' (Slide-seqV2), all observations must contain the same value."
+    
