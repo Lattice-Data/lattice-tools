6566215d4324643754b7d91f4256559ab376e3fc
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2024-05-22 12:06:54 -0700

    added several more terms for ontology bump tests

--- cellxgene_resources/cxg_5.1.0_testing/901_updated_ontologies_test.py
+++ cellxgene_resources/cxg_5.1.0_testing/901_updated_ontologies_test.py
@@ -17,7 +17,6 @@ from fixtures.valid_adatas import (
     validator_with_non_spatial_adata,
 )
 
-LIBRARY_ID = "spaceranger110_count_34914_WS_PLA_S9101764_GRCh38-3_0_0_premrna"
 
 # couldn't find an applicable EFO NTR that we might use, didn't seem to be any new assay terms
 @pytest.mark.parametrize(
@@ -26,6 +25,11 @@ LIBRARY_ID = "spaceranger110_count_34914_WS_PLA_S9101764_GRCh38-3_0_0_premrna"
         pytest.param("cell_type_ontology_term_id", "CL:4042002", id="TAC3-positive medium spiny neuron"),
         pytest.param("disease_ontology_term_id", "MONDO:0958349", id="dorsal spinal cord lipoma"),
         pytest.param("tissue_ontology_term_id", "UBERON:8600048", id="apex of urinary bladder"),
+        # clonal hematopoiesis terms applicable to one published collection
+        # https://github.com/chanzuckerberg/single-cell-curation/issues/825
+        pytest.param("disease_ontology_term_id", "MONDO:0100542", id="clonal hematopoiesis"),
+        pytest.param("disease_ontology_term_id", "MONDO:0100544", id="age-related clonal hematopoiesis"),
+        pytest.param("disease_ontology_term_id", "MONDO:0100543", id="clonal hematopoiesis of indeterminate potential"),
     )
 )
 def test_new_ontology_terms(validator_with_non_spatial_adata, col, term):
