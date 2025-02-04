68c0bf1a8bd725f053ab85a87251b5c245705913
Jchaffer787 <77802611+Jchaffer787@users.noreply.github.com>
2025-02-04 15:25:58 -0800

    Minor adjustments to make sure all visium files are tested and library_id is properly set

--- cellxgene_resources/cxg_schema5_testing/5-3_pr1196_cxg578_obs_array_row_col_in_tissue_nan_test.py
+++ cellxgene_resources/cxg_schema5_testing/5-3_pr1196_cxg578_obs_array_row_col_in_tissue_nan_test.py
@@ -8,14 +8,15 @@ import numpy as np
 import pandas as pd
 import pytest
 from fixtures.valid_adatas import (
-    validator_with_visium,
+    validator_with_all_visiums,
     validator_with_non_visium_adatas
 )
 
-LIBRARY_ID = "spaceranger110_count_34914_WS_PLA_S9101764_GRCh38-3_0_0_premrna"
+def get_library_id(adata):
+    return [key for key in adata.uns['spatial'].keys() if 'is_single' not in key][0]
 
-def test_array_col_passes(validator_with_visium):
-    validator = validator_with_visium
+def test_array_col_passes(validator_with_all_visiums):
+    validator = validator_with_all_visiums
     validator.validate_adata()
     assert validator.is_valid
     assert validator.errors == []
@@ -24,8 +25,8 @@ def test_array_col_passes(validator_with_visium):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_array_col_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_array_col_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[0], "array_col"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -37,8 +38,8 @@ def test_array_col_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_array_row_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_array_row_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[0], "array_row"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -50,8 +51,8 @@ def test_array_row_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_in_tissue_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_in_tissue_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[0], "in_tissue"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -63,8 +64,8 @@ def test_in_tissue_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_array_col_all_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_array_col_all_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[:], "array_col"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -76,8 +77,8 @@ def test_array_col_all_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_array_row_all_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_array_row_all_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[:], "array_row"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -89,8 +90,8 @@ def test_array_row_all_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_in_tissue_all_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_in_tissue_all_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[:], "in_tissue"] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -102,8 +103,8 @@ def test_in_tissue_all_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_multiple_contain_nan(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_multiple_contain_nan(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[0], "array_row"] = obs_value
     validator.adata.obs.loc[validator.adata.obs.index[0], "in_tissue"] = obs_value
     validator.validate_adata()
@@ -123,8 +124,8 @@ def test_multiple_contain_nan(validator_with_visium, obs_value):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_all_contain_nan(validator_with_visium, obs_value, obs_col):
-    validator = validator_with_visium
+def test_all_contain_nan(validator_with_all_visiums, obs_value, obs_col):
+    validator = validator_with_all_visiums
     validator.adata.obs.loc[validator.adata.obs.index[0], obs_col] = obs_value
     validator.validate_adata()
     assert validator.is_valid is False
@@ -134,8 +135,8 @@ def test_all_contain_nan(validator_with_visium, obs_value, obs_col):
     ]
 
 
-def test_array_col_missing(validator_with_visium):
-    validator = validator_with_visium
+def test_array_col_missing(validator_with_all_visiums):
+    validator = validator_with_all_visiums
     del validator.adata.obs['array_col']
     validator.validate_adata()
     assert validator.is_valid is False
@@ -144,8 +145,8 @@ def test_array_col_missing(validator_with_visium):
         "'EFO:0010961' (Visium Spatial Gene Expression) and uns['spatial']['is_single'] is True."
     ]
 
-def test_array_row_missing(validator_with_visium):
-    validator = validator_with_visium
+def test_array_row_missing(validator_with_all_visiums):
+    validator = validator_with_all_visiums
     del validator.adata.obs['array_row']
     validator.validate_adata()
     assert validator.is_valid is False
@@ -154,8 +155,8 @@ def test_array_row_missing(validator_with_visium):
         "'EFO:0010961' (Visium Spatial Gene Expression) and uns['spatial']['is_single'] is True."
     ]
 
-def test_in_tissue_missing(validator_with_visium):
-    validator = validator_with_visium
+def test_in_tissue_missing(validator_with_all_visiums):
+    validator = validator_with_all_visiums
     del validator.adata.obs['in_tissue']
     validator.validate_adata()
     assert validator.is_valid is False
@@ -168,10 +169,11 @@ def test_in_tissue_missing(validator_with_visium):
     "obs_value", (None, np.nan, float('nan'), float('NaN'), float('NAN'))
 )
 
-def test_obsm_visium_is_single_false_with_nans(validator_with_visium, obs_value):
-    validator = validator_with_visium
+def test_obsm_visium_is_single_false_with_nans(validator_with_all_visiums, obs_value):
+    validator = validator_with_all_visiums
     validator.adata.uns["spatial"]["is_single"] = False
-    del validator.adata.uns["spatial"][LIBRARY_ID]
+    library_id = get_library_id(validator.adata)
+    del validator.adata.uns["spatial"][library_id]
     validator.adata.obs.loc[validator.adata.obs.index[0], "array_col"] = obs_value
     validator.adata.obs.loc[validator.adata.obs.index[0], "array_row"] = obs_value
     validator.adata.obs.loc[validator.adata.obs.index[0], "in_tissue"] = obs_value
