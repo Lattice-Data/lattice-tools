512c889fa2c1ceba8b57722aad61d0dd0aa06798
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2025-02-05 11:06:33 -0800

    update label writing basics for multispecies tests

--- cellxgene_resources/cxg_schema5_testing/schema_reference_test.py
+++ cellxgene_resources/cxg_schema5_testing/schema_reference_test.py
@@ -1,7 +1,12 @@
 import tempfile
-from fixtures.valid_adatas import validator_with_all_adatas, label_writer
+from fixtures.valid_adatas import (
+    validator_with_all_adatas, 
+    validator_with_multispecies_adatas,
+    label_writer_multispecies,
+    label_writer,
+) 
 
-SCHEMA_VERSION = "5.1.0"
+SCHEMA_VERSION = "5.3.0"
 SCHEMA_REFERENCE = f"https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/{SCHEMA_VERSION}/schema.md"
 
 
@@ -21,3 +26,14 @@ def test_label_write_version_and_reference_is_correct(label_writer):
         assert label_writer.adata.uns["schema_reference"] == SCHEMA_REFERENCE
         assert label_writer.was_writing_successful
         assert not label_writer.errors
+
+
+def test_label_write_multispecies(label_writer_multispecies):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        labels_path = temp_dir + "labels.h5ad"
+        write_successful = label_writer_multispecies.write_labels(labels_path)
+
+        print(label_writer_multispecies.adata.var)
+        print(label_writer_multispecies.adata.raw.var)
+        assert not label_writer_multispecies.errors
+        assert write_successful
