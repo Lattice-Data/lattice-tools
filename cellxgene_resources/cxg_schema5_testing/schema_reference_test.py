f24fa0b122bdf0fe3f3136ba74d9126316b571b5
brian-mott <125435496+brian-mott@users.noreply.github.com>
2025-03-13 20:52:13 +0000

    refactor other misc-named tests

--- cellxgene_resources/cxg_schema5_testing/schema_reference_test.py
+++ cellxgene_resources/cxg_schema5_testing/schema_reference_test.py
@@ -1,8 +1,8 @@
 import tempfile
 from fixtures.valid_adatas import (
-    validator_with_all_adatas, 
-    validator_with_multispecies_adatas,
-    label_writer_multispecies,
+    test_h5ads,
+    validator_with_adatas, 
+    AnnDataLabelAppender,
     label_writer,
 ) 
 
@@ -10,8 +10,8 @@ SCHEMA_VERSION = "5.3.0"
 SCHEMA_REFERENCE = f"https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/{SCHEMA_VERSION}/schema.md"
 
 
-def test_validator_schema_version_is_correct(validator_with_all_adatas):
-    validator = validator_with_all_adatas
+def test_validator_schema_version_is_correct(validator_with_adatas):
+    validator = validator_with_adatas
     validator.validate_adata()
     assert validator.is_valid is True
     assert validator.schema_version == SCHEMA_VERSION
@@ -20,20 +20,18 @@ def test_validator_schema_version_is_correct(validator_with_all_adatas):
 def test_label_write_version_and_reference_is_correct(label_writer):
     with tempfile.TemporaryDirectory() as temp_dir:
         labels_path = temp_dir + "labels.h5ad"
-        label_writer.write_labels(labels_path)
+        write_successful = label_writer.write_labels(labels_path)
 
         assert label_writer.adata.uns["schema_version"] == SCHEMA_VERSION
         assert label_writer.adata.uns["schema_reference"] == SCHEMA_REFERENCE
-        assert label_writer.was_writing_successful
+        assert write_successful
         assert not label_writer.errors
 
 
-def test_label_write_multispecies(label_writer_multispecies):
+def test_label_write_general_pass(label_writer):
     with tempfile.TemporaryDirectory() as temp_dir:
         labels_path = temp_dir + "labels.h5ad"
-        write_successful = label_writer_multispecies.write_labels(labels_path)
+        write_successful = label_writer.write_labels(labels_path)
 
-        print(label_writer_multispecies.adata.var)
-        print(label_writer_multispecies.adata.raw.var)
-        assert not label_writer_multispecies.errors
         assert write_successful
+        assert not label_writer.errors
