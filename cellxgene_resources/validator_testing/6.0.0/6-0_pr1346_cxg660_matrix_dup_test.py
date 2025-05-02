b30eef3c6e7ceb9ceeefa7cade683caec8b05f56
corismall <corinn.small@gmail.com>
2025-05-01 20:55:34 -0700

    wip: add function for creating dups and initial test for matrix dups

--- cellxgene_resources/validator_testing/6.0.0/6-0_pr1346_cxg660_matrix_dup_test.py
+++ cellxgene_resources/validator_testing/6.0.0/6-0_pr1346_cxg660_matrix_dup_test.py
@@ -1,24 +1,46 @@
 """
 QA testing for this issue: https://app.zenhub.com/workspaces/single-cell-5e2a191dad828d52cc78b028/issues/gh/chanzuckerberg/single-cell-curation/1317
 PR: https://github.com/chanzuckerberg/single-cell-curation/pull/1346
+
+should not pass:
+ - duplicate a cell’s raw counts once (raw is in .X / raw is in raw.X)
+ - duplicate many cell's raw counts (raw is in .X / raw is in raw.X)
+ - duplicate a cell's raw counts many times (raw is in .X / raw is in raw.X)
+
+ @pytest.mark.parametrize("error", ERROR_MESSAGES)
 """
 
 import pytest
+from testing_internals.create_duplications import create_duplications
 from fixtures.valid_adatas import (
     ALL_H5ADS,
     test_h5ads,
     validator_with_adatas
 )
 
+ERROR_MESSAGES = (
+    #*** NEED TO FILL IN ***
+)
 
 
+@pytest.mark.parametrize("test_h5ads", ALL_H5ADS)
+def test_valid(validator_with_adatas):
+        validator = validator_with_adatas
+        validator.validate_adata()
+        assert validator.is_valid
+        assert validator.errors == []
 
 
-# should not pass:
-# - duplicate a cell’s raw counts once (raw is in .X / raw is in raw.X)
-# - duplicate many cell's raw counts (raw is in .X / raw is in raw.X)
-# - duplicate a cell's raw counts many times (raw is in .X / raw is in raw.X)
-#
-#
-#
-#
+@pytest.mark.parametrize("test_h5ads", ALL_H5ADS)
+def test_one_duplication(validator_with_adatas):
+        validator = validator_with_adatas
+        barcode = validator.adata.obs.index[0]
+        print(barcode)
+        dup_adata = create_duplications(validator.adata, i=barcode,n=1)
+        if validator.adata.raw:
+            validator.adata.raw = dup_adata
+        else:
+            validator.adata = dup_adata
+        validator.validate_adata()
+        assert not validator.is_valid
+
