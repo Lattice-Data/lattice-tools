459cedf7a1951a6a5e963c88b1fd40e7ba28d4fa
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2025-05-02 15:06:09 -0700

    other matrix functions converted to use dask

--- cellxgene_resources/cellxgene_mods.py
+++ cellxgene_resources/cellxgene_mods.py
@@ -12,6 +12,7 @@ import scanpy as sc
 import squidpy as sq
 import subprocess
 import sys
+from anndata.compat import DaskArray
 from dataclasses import dataclass
 from dask.array import map_blocks
 from pathlib import Path
@@ -20,6 +21,9 @@ from cellxgene_ontology_guide.ontology_parser import OntologyParser
 import cellxgene_schema.gencode as gencode
 import cellxgene_schema.utils as utils
 import cellxgene_schema.schema as schema
+from typing import Union
+from cellxgene_schema.utils import SPARSE_MATRIX_TYPES
+
 
 portal_uns_fields = [
     'citation',
@@ -189,76 +193,132 @@ def calculate_adata_memory(adata_path: str, print_datasets: bool = False, sizes:
     return sizes
 
 
-def determine_sparsity(x):
-    if isinstance(x, sparse.coo_matrix) or isinstance(x, sparse.csr_matrix) or isinstance(x, sparse.csc_matrix):
-        sparsity = 1 - x.count_nonzero() / float(np.cumprod(x.shape)[-1])
-    elif isinstance(x, np.ndarray):
-        sparsity = 1 - np.count_nonzero(x) / float(np.cumprod(x.shape)[-1])
-    else:
-        report(f'matrix is of type {type(x)}, sparsity calculation has not been implemented')
+def get_matrix_format(matrix: DaskArray) -> str:
+    """
+    Given a matrix, returns the format as one of: csc, csr, coo, dense
+    or unknown.
 
+    This mimics the scipy.sparse `format` property, but extends it to
+    support ndarray and other classes AnnData may proxy the matrix with.
+    """
+
+    # Note: the AnnData proxy classes DO support the `format_str` property, but
+    # doing a slice seemed safer, if less performant.  Using `format_str`, which
+    # currently works, uses private API:
+    #
+    # >>> return getattr(matrix, "format_str", "dense)
+    #
+    matrix_format = "unknown"
+    try:
+        matrix_slice = matrix[0:1, 0:1].compute()
+    except AttributeError:
+        # compute() may fail on an unknown matrix value. if so, return "unknown"
+        return matrix_format
+    if isinstance(matrix_slice, sparse.spmatrix):
+        matrix_format = matrix_slice.format
+    elif isinstance(matrix_slice, np.ndarray):
+        matrix_format = "dense"
+    assert matrix_format in SPARSE_MATRIX_TYPES.union({"unknown", "dense"})
+    return matrix_format
+
+
+def count_matrix_nonzero(matrix: DaskArray) -> int:
+    def count_nonzeros(matrix_chunk: Union[np.ndarray, sparse.spmatrix], is_sparse_matrix: bool) -> np.array:
+        nnz = matrix_chunk.nnz if is_sparse_matrix else np.count_nonzero(matrix_chunk)
+        return np.array([nnz])
+
+    is_sparse_matrix = get_matrix_format(matrix) in SPARSE_MATRIX_TYPES
+    # if matrix too small to chunk, then just call function and don't dask distribute
+    if len(matrix.chunks[0]) > 1:
+        nonzeros = map_blocks(
+            count_nonzeros,
+            matrix, 
+            is_sparse_matrix, 
+            drop_axis=1, 
+            dtype=int) \
+                .compute(scheduler='processes') \
+                .sum()
+    else:
+        nonzeros = count_nonzeros(matrix.compute(), is_sparse_matrix)[0]
+    return nonzeros
+
+
+def determine_sparsity(matrix: DaskArray):
+    nnz = count_matrix_nonzero(matrix)
+    sparsity = 1 - nnz / np.prod(matrix.shape)
     return round(sparsity, 3)
 
 
-def evaluate_sparsity(adata):
-    max_sparsity = 0.5
+def get_matrices_to_evaluate(adata: ad.AnnData) -> list[tuple[Union[np.ndarray, sparse.spmatrix], str]]:
+    # will always be adata.X, then add other matrices if they exist
+    matrices_to_evaluate = [(adata.X, "X")]
 
-    valid = True
-    sparsity = determine_sparsity(adata.X)
-    report(f'X sparsity: {sparsity}')
-    if sparsity > max_sparsity and type(adata.X) != sparse.csr_matrix:
-        report('X should be converted to csr sparse', 'ERROR')
-        valid = False
-    
     if adata.raw:
-        sparsity = determine_sparsity(adata.raw.X)
-        report(f'raw.X sparsity: {sparsity}')
-        if sparsity > max_sparsity and type(adata.raw.X) != sparse.csr_matrix:
-            report('raw.X should be converted to csr sparse', 'ERROR')
-            valid = False
+        matrices_to_evaluate.append((adata.raw.X, "raw.X"))
+    for layer in adata.layers:
+        matrices_to_evaluate.append((adata.layers[layer], f"layer '{layer}'"))
     
-    for l in adata.layers:
-        sparsity = determine_sparsity(adata.layers[l])
-        report(f'layers[{l}] sparsity: {sparsity}')
-        if sparsity > max_sparsity and type(adata.layers[l]) != sparse.csr_matrix:
-            report(f'layers[{l}] should be converted to csr sparse', 'ERROR')
+    return matrices_to_evaluate
+
+
+def evaluate_sparsity(adata: ad.AnnData):
+    max_sparsity = 0.5
+    valid = True
+
+    # will always be adata.X, then add other matrices if they exist
+    matrices_to_evaluate = get_matrices_to_evaluate(adata)
+
+    for matrix, matrix_name in matrices_to_evaluate:
+        format = get_matrix_format(matrix)
+        sparsity = determine_sparsity(matrix)
+        report(f"{matrix_name} sparsity: {sparsity}")
+        if sparsity > max_sparsity and format not in SPARSE_MATRIX_TYPES:
+            report(f'{matrix_name} should be converted to csr sparse, found to be {format}', 'ERROR')
             valid = False
 
     if valid:
-        report('all matrices have passed checks', 'GOOD')
+        report("All matrices have passed checks", "GOOD")
 
 
 def evaluate_data(adata):
     min_maxs = {}
-    if adata.raw:
-        raw_min = adata.raw.X.min()
-        raw_max = adata.raw.X.max()
-        report(f'raw min = {raw_min}')
-        report(f'raw max = {raw_max}')
-        min_maxs['raw'] = f'{raw_min}-{raw_max}'
-        all_integers = np.all(np.round(adata.raw.X.data) == adata.raw.X.data)
-    else:
-        all_integers = np.all(np.round(adata.X.data) == adata.X.data)
+    matrices_to_evaluate = get_matrices_to_evaluate(adata)
+    matrix_names = [name for _, name in matrices_to_evaluate]
+    raw_matrix = "raw.X" if "raw.X" in matrix_names else "X"
+    
+    def get_max_chunk(matrix_chunk: sparse.spmatrix) -> np.array:
+        return np.array([matrix_chunk.max()]).reshape(-1, 1)
+        
+    def get_min_chunk(matrix_chunk: sparse.spmatrix) -> np.array:
+        return np.array([matrix_chunk.min()]).reshape(-1, 1)
 
-    if all_integers:
-        report('raw is all integers', 'GOOD')
-    else:
-        report('raw contains non-integer values', 'ERROR')
+    def all_integers_chunk(matrix_chunk: sparse.spmatrix) -> np.array:
+        data_array = matrix_chunk.data
+        is_all_integers = np.all(np.round(data_array) == data_array)
+        return np.array([is_all_integers]).reshape(-1, 1)
 
-    X_min = adata.X.min()
-    X_max = adata.X.max()
-    report(f'X min = {X_min}')
-    report(f'X max = {X_max}')
-    min_maxs['X'] = f'{X_min}-{X_max}'
 
-    for l in adata.layers:
-        min = adata.layers[l].min()
-        max = adata.layers[l].max()
-        report(f'layers[{l}] min = {min}')
-        report(f'layers[{l}] max = {max}')
-        min_maxs[l] = f'{min}-{max}'
+    for matrix, matrix_name in matrices_to_evaluate:
+        with dask.config.set(scheduler='processes'):
+            max = map_blocks(get_max_chunk, matrix, dtype=int).compute().ravel().max()
+            min = map_blocks(get_min_chunk, matrix, dtype=int).compute().ravel().min()
+            if matrix_name == raw_matrix:
+                is_all_integers = map_blocks(all_integers_chunk, matrix, dtype=bool).compute().ravel().all()
+
+        min_maxs[matrix_name] = f"{min}-{max}"
+
+        report(f"{matrix_name} min = {min}")
+        report(f"{matrix_name} max = {max}")
+
+        if matrix_name == raw_matrix:
+            if is_all_integers:
+                report(f"{matrix_name} is all integers", "GOOD")
+            else:
+                report(f"{matrix_name} is NOT all integers", "ERROR")
+            
 
     poss_dups = [k for k,v in min_maxs.items() if list(min_maxs.values()).count(v) > 1]
+
     if poss_dups:
         report(f'possible redundant layers: {poss_dups}','WARNING')
 
@@ -289,7 +349,6 @@ def evaluate_uns_colors(adata):
                     report(f'uns.{k} defined appropriately', 'GOOD')
     else:
         report('no _colors keys defined')
-
 
 
 def map_filter_gene_ids(adata):
@@ -459,70 +518,76 @@ def evaluate_obs(obs, full_obs_standards):
         report(f'long fields: {long_fields}')
 
 
-def evaluate_dup_counts(adata):
+def evaluate_dup_counts(adata: ad.AnnData, worker_type="processes") -> pd.DataFrame:
     """
-    Hash sparse csr matrix using np.ndarrays that represent sparse matrix data.
-    First pass will hash all rows via slicing the data array and append to copy of obs df
-    Second pass will hash only duplicate rows in obs copy via the indices array.
-    This will keep only true duplicated matrix rows and not rows with an indicental same
-    ordering of their data arrays
+    Hash raw counts matrix in parallel with dask
+    If there are duplicated rows, return a copy of obs with the duplicate rows' metadata
+    
+    This uses single-cell-curation read_h5ad() to lazily load matrices as dask arrays
+    Dask will schedule out chunks to workers (processes in this case) and return a 
+    properly ordered array of hashes
+
+    Key is to reshape the chunking function output to a columnar array with .reshape() 
+    and then use .ravel() after calling compute to flatten the final desired array. 
+    This avoids exceptions with broadcasting for the final chunk that != chunk_size
+
+    Joyce did further work for the validator implementation to show that the sha224
+    hash seems to be quickest
+    
+    Uses presence of obs "in_tissue" column to default to dense array hashing for
+    visium datasets; this should prevent false positives for spots at the edge of the 
+    tissue borders. A further filtration step will remove obs rows with in_tissue==0
+
+    Stupidly fast and efficient; on M1 Max with 64 GB, can hash the largest dataset
+    (11.4 million cells) in about 3 1/2 minutes. The r5 16xlarge EC2 will take about a
+    minute to 1:15, depending on chunk size. Memory usage is minimal when slicing through
+    the data array.
+
+    This may not work in a script, but storing here for the moment to be in the commit
+    history
+
+    Actually this does work when imported into a notebook
     """
-    if 'in_tissue' in adata.obs.columns:
-        obs_to_keep = adata.obs[adata.obs['in_tissue'] != 0].index
-        adata = adata[obs_to_keep, : ]
 
     matrix = adata.raw.X if adata.raw else adata.X
 
-    if not isinstance(matrix, sparse.csr_matrix):
-        print("Matrix not in sparse csr format, please convert before hashing")
-        return
+    def hash_data_array_chunk(matrix_chunk) -> np.array:
+        data_array = matrix_chunk.data
+        indptr_array = matrix_chunk.indptr
 
-    nnz = matrix.nnz
+        start, end = 0, matrix_chunk.shape[0]
+        chunk_hashes = []
+        while start < end:
+            val = hashlib.sha224(data_array[indptr_array[start]:indptr_array[start + 1]].tobytes()).hexdigest()
+            chunk_hashes.append(val)
+            start += 1
+            
+        return np.array([chunk_hashes]).reshape(-1, 1)
 
-    if not matrix.has_canonical_format:
-        print("Csr matrix not in canonical format, converting now...")
-        if adata.raw:
-            adata.raw.X.sort_indices()
-            adata.raw.X.sum_duplicates()
-        else:
-            adata.X.sort_indices()
-            adata.X.sum_duplicates()
-
-    assert matrix.has_canonical_format, "Matrix still in non-canonical format"
-
-    if nnz != matrix.nnz:
-        print(f"{nnz - matrix.nnz} duplicates found during canonical conversion")
+        
+    def hash_dense_chunk(matrix_chunk) -> np.array:
+        chunk_hashes = [hashlib.sha224(r.tobytes()).hexdigest() for r in matrix_chunk.toarray()]
+        return np.array([chunk_hashes]).reshape(-1, 1)
 
 
-    data_array = matrix.data
-    index_array = matrix.indices
-    indptr_array = matrix.indptr
+    hash_chunk_func = hash_dense_chunk if "in_tissue" in adata.obs.columns else hash_data_array_chunk
 
-    start, end = 0, matrix.shape[0]
-    hashes = []
-    while start < end:
-        val = hash(data_array[indptr_array[start]:indptr_array[start + 1]].tobytes())
-        hashes.append(val)
-        start += 1
+    with dask.config.set(scheduler=worker_type):
+        hashes = map_blocks(hash_chunk_func, matrix, dtype=int).compute().ravel()
 
-    def index_hash(index):
-        obs_loc = adata.obs.index.get_loc(index)
-        val = hash(index_array[indptr_array[obs_loc]:indptr_array[obs_loc + 1]].tobytes())
-
-        return val
-    
     hash_df = adata.obs.copy()
-    hash_df['data_array_hash'] = hashes
-    hash_df = hash_df[hash_df.duplicated(subset='data_array_hash',keep=False) == True]
-    hash_df.sort_values('data_array_hash', inplace=True)
+    hash_df["row_hash"] = hashes
 
-    hash_df['index_array_hash'] = [index_hash(row) for row in hash_df.index.to_list()]
-    hash_df = hash_df[hash_df.duplicated(subset=['data_array_hash', 'index_array_hash'], keep=False) == True]
+    if "in_tissue" in hash_df.columns:
+        obs_to_keep = hash_df[hash_df["in_tissue"] != 0].index
+        hash_df = hash_df[hash_df.index.isin(obs_to_keep)]
 
-    if not hash_df.empty:
-        report('duplicated raw counts', 'ERROR')
-        return hash_df
-    report('no duplicated raw counts', 'GOOD')
+    dup_df = hash_df[hash_df.duplicated(subset="row_hash", keep=False)].copy()
+
+    if not dup_df.empty:
+        report("duplicated raw counts", "ERROR")
+        return dup_df
+    report("no duplicated raw counts", "GOOD")
 
     
 def symbols_to_ids(symbols, var):
