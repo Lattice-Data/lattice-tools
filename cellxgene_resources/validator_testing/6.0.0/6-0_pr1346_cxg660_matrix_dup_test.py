a025ea594567c248b5452ecd935a8d1993e2dfa5
corismall <corinn.small@gmail.com>
2025-05-02 10:04:04 -0700

    wip: separating spatial & non-spatial

--- cellxgene_resources/validator_testing/6.0.0/6-0_pr1346_cxg660_matrix_dup_test.py
+++ cellxgene_resources/validator_testing/6.0.0/6-0_pr1346_cxg660_matrix_dup_test.py
@@ -7,15 +7,17 @@ should not pass:
  - duplicate many cell's raw counts (raw is in .X / raw is in raw.X)
  - duplicate a cell's raw counts many times (raw is in .X / raw is in raw.X)
 
- @pytest.mark.parametrize("error", ERROR_MESSAGES)
 """
 
 import pytest
 from testing_internals.create_duplications import create_duplications
 from fixtures.valid_adatas import (
     ALL_H5ADS,
+    NON_SPATIAL_H5ADS,
+    SPATIAL_H5ADS,
     test_h5ads,
     validator_with_adatas
+
 )
 
 ERROR_MESSAGES = (
@@ -25,22 +27,41 @@ ERROR_MESSAGES = (
 
 @pytest.mark.parametrize("test_h5ads", ALL_H5ADS)
 def test_valid(validator_with_adatas):
-        validator = validator_with_adatas
-        validator.validate_adata()
-        assert validator.is_valid
-        assert validator.errors == []
+    validator = validator_with_adatas
+    validator.validate_adata()
+    assert validator.is_valid
+    assert validator.errors == []
 
 
-@pytest.mark.parametrize("test_h5ads", ALL_H5ADS)
-def test_one_duplication(validator_with_adatas):
-        validator = validator_with_adatas
-        barcode = validator.adata.obs.index[0]
-        print(barcode)
-        dup_adata = create_duplications(validator.adata, i=barcode,n=1)
-        if validator.adata.raw:
-            validator.adata.raw = dup_adata
-        else:
-            validator.adata = dup_adata
-        validator.validate_adata()
-        assert not validator.is_valid
+@pytest.mark.parametrize("test_h5ads", NON_SPATIAL_H5ADS)
+@pytest.mark.parametrize("error", ERROR_MESSAGES)
+def test_one_duplication_non_visium(validator_with_adatas,error):
+    validator = validator_with_adatas
+    barcode = validator.adata.obs.index[0]
+    print(barcode)
+    dup_adata = create_duplications(validator.adata, i=barcode,n=1)
+    if validator.adata.raw:
+        validator.adata.raw = dup_adata
+    else:
+        validator.adata = dup_adata
+    validator.validate_adata()
+    assert not validator.is_valid
 
+
+@pytest.mark.parametrize("test_h5ads", SPATIAL_H5ADS)
+@pytest.mark.parametrize("error", ERROR_MESSAGES)
+def test_one_duplication_visium(validator_with_adatas,error):
+    validator = validator_with_adatas
+    if "in_tissue" in validator.adata.obs.columns:
+        obs_to_keep = validator.adata.obs[validator.adata.obs["in_tissue"] != 0].index
+
+    validator.adata = validator.adata[obs_to_keep, :]
+    barcode = validator.adata.obs.index[0]
+    print(barcode)
+    dup_adata = create_duplications(validator.adata, i=barcode,n=1)
+    if validator.adata.raw:
+        validator.adata.raw = dup_adata
+    else:
+        validator.adata = dup_adata
+    validator.validate_adata()
+    assert not validator.is_valid
