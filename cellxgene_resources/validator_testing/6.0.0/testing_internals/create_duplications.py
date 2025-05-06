7f5a93a77d9f8428aa0bbc9f97e8c4ba6c31c259
corismall <corinn.small@gmail.com>
2025-05-05 18:46:10 -0700

    completed function

--- cellxgene_resources/validator_testing/6.0.0/testing_internals/create_duplications.py
+++ cellxgene_resources/validator_testing/6.0.0/testing_internals/create_duplications.py
@@ -1,36 +1,62 @@
 import anndata as ad
-import random
+import numpy as np
+import dask.array as da
 
-def create_duplications(adata:ad.AnnData,i:str,n=int):
+
+def create_duplications(adata:ad.AnnData,barcodes:list[str],n=int):
         '''
-        Duplicates raw cell counts within a valid h5ad by choosing a number of random rows and replacing them with a specified index.
-        i = index to duplicate; n = number of times to duplicate
+        Duplicates raw cell counts within a valid h5ad by choosing random rows and replacing them with a specified index.
+        barcodes = list of indices to duplicate; n = number of times to duplicate each barcode
 
         :rtype Raw Anndata with duplications.
         '''
-        print(adata.shape)
-        obs_loc = adata.obs.index.get_loc(i)
-        obs_row_for_dup = adata.obs.iloc[obs_loc].copy()
-        random_indices = [random.randint(1, adata.obs.shape[0]-1) for m in range(n)]
-        print(f"Replacing rows:{random_indices} with row {obs_loc}")
-        for o_indx in random_indices:
-            print(o_indx)
-            adata.obs.iloc[o_indx] = obs_row_for_dup.values
+        list_for_obs = []
+        total_needed = n * len(barcodes)
+        if total_needed > len(adata.obs):
+                raise ValueError("Not enough rows in the DataFrame to sample without replacement.")
 
-        to_validate=[]
+        candidate_indices = adata.obs.index[~adata.obs.index.isin(barcodes)].tolist()
+        random_indices = np.random.choice(candidate_indices, size=total_needed, replace=False)  # Sample total unique indices minus those specified in barcodes list
+        random_indices_list = [random_indices[i * n:(i + 1) * n].tolist() for i in range(len(barcodes))]
+        obs_name_to_index = {name: i for i, name in enumerate(adata.obs_names)}
+
+        for barcode, target_barcodes in zip(barcodes, random_indices_list):
+                obs_indx = obs_name_to_index[barcode]
+                obs_meta = adata.obs.iloc[obs_indx].copy()
+                list_for_obs.append([barcode,obs_indx,obs_meta,target_barcodes])
+
+        dict_for_mtx = {}
+        count = 0
+        for barcode,obs_indx,obs_meta,target_barcodes in list_for_obs:
+                for tb in target_barcodes:
+                        adata.obs.loc[tb] = obs_meta
+                target_indices = [obs_name_to_index[name] for name in target_barcodes]
+                dict_for_mtx[f"indices_{count}"] = (obs_indx,target_indices)
+                count += 1
 
         if adata.raw:
-                print("Raw counts found in adata.raw")
-                to_validate.append(adata.raw)
+                dict_for_mtx["raw_adata"] = adata.raw
         else:
-                print("Raw counts found in adata")
-                to_validate.append(adata)
+                dict_for_mtx["raw_adata"] = adata
 
-        for raw_adata in to_validate:
-                raw_matrix = raw_adata.X
-                raw_matrix_row = raw_matrix[obs_loc, :]
-                for m_indx in random_indices:
-                        raw_matrix[m_indx] = raw_matrix_row
+        raw_matrix = dict_for_mtx["raw_adata"].X
+        is_dask = hasattr(raw_matrix, "compute")
+        if is_dask:
+                for key,value in dict_for_mtx.items():
+                        if "indices" in key:
+                                raw_matrix_row = raw_matrix[value[0], :].compute()
+                                raw_matrix_numpy = raw_matrix.compute()
+                                original_chunks = raw_matrix.chunks
+                                for i in value[1]:
+                                        raw_matrix_numpy[i] = raw_matrix_row
+                                raw_matrix = da.from_array(raw_matrix_numpy, chunks=original_chunks)
 
-                raw_adata = ad.AnnData(raw_matrix, obs=adata.obs, var=adata.var, uns=adata.uns, obsm=adata.obsm)
-                return raw_adata
+        else:
+                for key,value in dict_for_mtx.items():
+                        if "indices" in key:
+                                raw_matrix_row = raw_matrix[value[0], :]
+                                for i in value[1]:
+                                        raw_matrix[i] = raw_matrix_row
+
+        raw_adata = ad.AnnData(raw_matrix, obs=adata.obs, var=adata.var, uns=adata.uns, obsm=adata.obsm)
+        return raw_adata
