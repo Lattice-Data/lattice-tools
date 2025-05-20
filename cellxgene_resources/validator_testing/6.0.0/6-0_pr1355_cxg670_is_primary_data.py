6509762d74ecb1c608585e84c9ee2de5cd295cc1
corismall <corinn.small@gmail.com>
2025-05-20 10:15:53 -0700

    consolidating tests

--- cellxgene_resources/validator_testing/6.0.0/6-0_pr1355_cxg670_is_primary_data.py
+++ cellxgene_resources/validator_testing/6.0.0/6-0_pr1355_cxg670_is_primary_data.py
@@ -6,16 +6,6 @@ Testing conditions for is_single = False (non-spatial and integrated visium data
 - valid_human.h5ad converted to integrated visium (is_single = False)
 - wild integrated visium datasets: https://cellxgene.cziscience.com/e/22ec5e4c-a2db-4769-8c4a-9530b5f6962d.cxg/ and https://cellxgene.cziscience.com/e/83ec9e14-87a4-4d41-aa3b-f7c51af70a64.cxg/
 
-##################################
--> ISSUE with submission of integrated visium datasets: are all dots included in integrated dataset?
-If so, edge dots with the low duplicate counts or non-tissue all zero counts will be included, and then fail the duplicate check,
-or are they filtering for only meaningful dots and the duplicate check should pass?
-
-    Currently:
-    - valid_human.h5ad doesn't include duplicates
-    - wild integrated visium datasets don't include duplicates
-##################################
-
 Should pass:
     - is_single: False (both bool and npbool), is_primary_data: False
 
@@ -50,12 +40,13 @@ class TestNonVisiumData:
         assert self.validator.errors == []
 
 
-    def test_nonV_all_false(self):
+    @pytest.mark.parametrize("is_single_value", [False, np.bool_(False)])
+    def test_nonV_all_false(self, is_single_value):
 
         # is_single: False (bool), is_primary_data: False -> pass
 
         self.validator.adata.obs["assay_ontology_term_id"] = "EFO:0022860"
-        self.validator.adata.uns["spatial"] = {'is_single': False}
+        self.validator.adata.uns["spatial"] = {'is_single': is_single_value}
         self.validator.adata.obs["is_primary_data"] = False
         self.validator.adata.obs["suspension_type"] = "na"
         self.validator.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
@@ -63,42 +54,13 @@ class TestNonVisiumData:
         assert self.validator.is_valid
         assert self.validator.errors == []
 
-
-    def test_nonV_all_false_npbool(self):
-
-        # is_single: False (npbool), is_primary_data: False -> pass
-
-        self.validator.adata.obs["assay_ontology_term_id"] = "EFO:0022860"
-        self.validator.adata.uns["spatial"] = {'is_single': np.bool_(False)}
-        self.validator.adata.obs["is_primary_data"] = False
-        self.validator.adata.obs["suspension_type"] = "na"
-        self.validator.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
-        self.validator.validate_adata()
-        assert self.validator.is_valid
-        assert self.validator.errors == []
-
-
-    def test_nonV_ipd_true(self):
+    @pytest.mark.parametrize("is_single_value", [False, np.bool_(False)])
+    def test_nonV_ipd_true(self, is_single_value):
 
         # is_single: False (bool), is_primary_data: True -> fail
 
         self.validator.adata.obs["assay_ontology_term_id"] = "EFO:0022860"
-        self.validator.adata.uns["spatial"] = {'is_single': False}
-        self.validator.adata.obs["is_primary_data"] = True
-        self.validator.adata.obs["suspension_type"] = "na"
-        self.validator.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
-        self.validator.validate_adata()
-        assert not self.validator.is_valid
-        assert (f"ERROR: When uns['spatial']['is_single'] is {self.validator.adata.uns['spatial']['is_single']}, obs['is_primary_data'] must be False for all rows."
-        ) in self.validator.errors
-
-
-    def test_nonV_ipd_true_npbool(self):
-
-        # is_single: False (npbool), is_primary_data: True -> fail
-
-        self.validator.adata.obs["assay_ontology_term_id"] = "EFO:0022860"
-        self.validator.adata.uns["spatial"] = {'is_single': np.bool_(False)}
+        self.validator.adata.uns["spatial"] = {'is_single': is_single_value}
         self.validator.adata.obs["is_primary_data"] = True
         self.validator.adata.obs["suspension_type"] = "na"
         self.validator.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
@@ -120,48 +82,25 @@ class TestVisiumData:
         assert self.validator.is_valid
         assert self.validator.errors == []
 
-
-    def test_valid_V_all_false(self):
+    @pytest.mark.parametrize("is_single_value", [False, np.bool_(False)])
+    def test_valid_V_all_false(self,is_single_value):
 
          # is_single: False (bool), is_primary_data: False -> pass
 
-        self.validator.adata.uns["spatial"]["is_single"] = False
+        self.validator.adata.uns["spatial"]["is_single"] = is_single_value
         self.validator.adata.obs["is_primary_data"] = False
         self.validator.validate_adata()
         assert self.validator.is_valid
         assert self.validator.errors == []
 
-
-    def test_valid_V_all_false_npbool(self):
-
-        # is_single: False (npbool), is_primary_data: False -> pass
-
-        self.validator.adata.uns["spatial"]["is_single"] = np.bool_(False)
-        self.validator.adata.obs["is_primary_data"] = False
-        self.validator.validate_adata()
-        assert self.validator.is_valid
-        assert self.validator.errors == []
-
-
-    def test_invalid_V_ipd_true(self):
+    @pytest.mark.parametrize("is_single_value", [False, np.bool_(False)])
+    def test_invalid_V_ipd_true(self, is_single_value):
 
         # is_single: False (bool), is_primary_data: True -> fail
 
-        self.validator.adata.uns["spatial"]["is_single"] = False
+        self.validator.adata.uns["spatial"]["is_single"] = is_single_value
         self.validator.adata.obs["is_primary_data"] = True
         self.validator.validate_adata()
         assert not self.validator.is_valid
         assert (f"ERROR: When uns['spatial']['is_single'] is {self.validator.adata.uns['spatial']['is_single']}, obs['is_primary_data'] must be False for all rows."
         ) in self.validator.errors
-
-
-    def test_invalid_V_ipd_true_npbool(self):
-
-        # is_single: False (npbool), is_primary_data: True -> fail
-
-        self.validator.adata.uns["spatial"]["is_single"] = np.bool_(False)
-        self.validator.adata.obs["is_primary_data"] = True
-        self.validator.validate_adata()
-        assert not self.validator.is_valid
-        assert (f"ERROR: When uns['spatial']['is_single'] is {self.validator.adata.uns['spatial']['is_single']}, obs['is_primary_data'] must be False for all rows."
-    ) in self.validator.errors
