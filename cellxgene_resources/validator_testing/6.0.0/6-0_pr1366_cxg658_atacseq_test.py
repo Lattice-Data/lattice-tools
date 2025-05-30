152bea4671f7d952ddd7f62a26394090b2dc9ab7
corismall <corinn.small@gmail.com>
2025-05-30 11:53:01 -0700

    remove validator code/add in error messages

--- cellxgene_resources/validator_testing/6.0.0/6-0_pr1366_cxg658_atacseq_test.py
+++ cellxgene_resources/validator_testing/6.0.0/6-0_pr1366_cxg658_atacseq_test.py
@@ -7,9 +7,9 @@ Should pass:
 (Y) mouse ATAC fragment w/ mouse h5ad
 
 Shouldn't pass:
-(N) ATAC fragment associated with a dataset with uns.organism_ontology_term_id on approved list but not human or mouse
-(N) mouse ATAC fragment w/ human h5ad -> no errors
-(N) human ATAC fragment w/ mouse h5ad -> no errors
+(Y) ATAC fragment associated with a dataset with uns.organism_ontology_term_id on approved list but not human or mouse
+(Y) mouse ATAC fragment w/ human h5ad
+(Y) human ATAC fragment w/ mouse h5ad
 """
 
 import anndata as ad
@@ -19,20 +19,16 @@ import numpy as np
 import pytest
 import pyarrow
 from cellxgene_schema.atac_seq import (
-    check_anndata_requires_fragment,
     process_fragment,
-    validate_anndata_with_fragment
 )
 from pandas._libs.parsers import STR_NA_VALUES
 from pathlib import Path
 from fixtures.create_fixtures import Organism
-from cellxgene_schema.validate import Validator
 from fixtures.valid_adatas import (
     ATAC_H5ADS,
     FIXTURES_ROOT,
     NON_SPATIAL_H5ADS,
     read_h5ad,
-    validator_with_adatas,
     yield_atac_fixture_data,
     yield_atac_h5ads,
     _to_anndata_file,
@@ -50,16 +46,6 @@ class TestHumanATACValidation:
     @pytest.fixture(autouse=True)
     def setup(self, yield_atac_fixture_data):
         self.atac = yield_atac_fixture_data
-        self.validator = Validator()
-        self.validator.adata = read_h5ad(FIXTURES_ROOT / self.atac.h5ad_file_name)
-
-
-    def test_valid_human(self):
-        print(self.atac)
-        print(self.validator.adata)
-        self.validator.validate_adata()
-        assert self.validator.is_valid
-        assert self.validator.errors == []
 
 
     @pytest.mark.parametrize("assay", ASSAYS)
@@ -67,11 +53,11 @@ class TestHumanATACValidation:
 
         # human ATAC fragment w/ human h5ad -> pass
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:9606" == organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
         temp_files = to_temp_files(self.atac, tmpdir)
         results = process_fragment(**temp_files)
@@ -81,21 +67,30 @@ class TestHumanATACValidation:
     @pytest.mark.parametrize("assay", ASSAYS)
     def test_mouse_atac_fragment_w_human_h5ad(self, assay, tmpdir):
 
-        # mouse ATAC fragment w/ human h5ad -> fail  ### doesn't error
+        # mouse ATAC fragment w/ human h5ad -> fail
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:9606" == organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
+        frag_df = pd.read_csv(str(FIXTURES_ROOT / f"valid_mouse_fragments.tsv.gz"), sep='\t',header=None)
+        self.atac.fragment_df = frag_df
         self.atac.fragment_file_name = "valid_mouse_fragments.tsv.gz"
-        print(self.atac.fragment_df)
         temp_files = to_temp_files(self.atac, tmpdir)
-        print(temp_files)
         results = process_fragment(**temp_files)
-        print(results)
         assert results != []
+        assert (
+            "Errors found in Fragment and/or Anndata file"
+        ) in results
+        assert (
+            "Barcodes don't match anndata.obs.index"
+        ) in results
+        assert (
+            f"Chromosomes in the fragment do not match the organism({organism})."
+        ) in [r for r in results if "Chromosomes" in r][0].split("\n")
+
 
 
 @pytest.mark.parametrize("yield_atac_h5ads", MOUSE_H5ADS)
@@ -103,16 +98,6 @@ class TestMouseATACValidation:
     @pytest.fixture(autouse=True)
     def setup(self, yield_atac_fixture_data):
         self.atac = yield_atac_fixture_data
-        self.validator = Validator()
-        self.validator.adata = read_h5ad(FIXTURES_ROOT / self.atac.h5ad_file_name)
-
-
-    def test_valid_mouse(self):
-        print(self.atac)
-        print(self.validator.adata)
-        self.validator.validate_adata()
-        assert self.validator.is_valid
-        assert self.validator.errors == []
 
 
     @pytest.mark.parametrize("assay", ASSAYS)
@@ -120,11 +105,11 @@ class TestMouseATACValidation:
 
         # mouse ATAC fragment w/ mouse h5ad -> pass
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:10090" == organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
         temp_files = to_temp_files(self.atac, tmpdir)
         results = process_fragment(**temp_files)
@@ -134,21 +119,29 @@ class TestMouseATACValidation:
     @pytest.mark.parametrize("assay", ASSAYS)
     def test_human_atac_fragment_w_mouse_h5ad(self, assay, tmpdir):
 
-        # human ATAC fragment w/ mouse h5ad -> fail ### doesnt errror
+        # human ATAC fragment w/ mouse h5ad -> fail
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:10090" == organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
+        frag_df = pd.read_csv(str(FIXTURES_ROOT / f"valid_human_fragments.tsv.gz"), sep='\t',header=None)
+        self.atac.fragment_df = frag_df
         self.atac.fragment_file_name = "valid_human_fragments.tsv.gz"
-        print(self.atac.fragment_df)
         temp_files = to_temp_files(self.atac, tmpdir)
-        print(temp_files)
         results = process_fragment(**temp_files)
-        print(results)
         assert results != []
+        assert (
+            "Errors found in Fragment and/or Anndata file"
+        ) in results
+        assert (
+            "Barcodes don't match anndata.obs.index"
+        ) in results
+        assert (
+            f"Chromosomes in the fragment do not match the organism({organism})."
+        ) in [r for r in results if "Chromosomes" in r][0].split("\n")
 
 
 @pytest.mark.parametrize("yield_atac_h5ads", NON_HUMAN_MOUSE_H5ADS)
@@ -156,38 +149,34 @@ class TestOtherATACValidation:
     @pytest.fixture(autouse=True)
     def setup(self, yield_atac_fixture_data):
         self.atac = yield_atac_fixture_data
-        self.validator = Validator()
-        self.validator.adata = read_h5ad(FIXTURES_ROOT / self.atac.h5ad_file_name)
-
-
-    def test_valid_other(self):
-        print(self.atac)
-        print(self.validator.adata)
-        self.validator.validate_adata()
-        assert self.validator.is_valid
-        assert self.validator.errors == []
 
 
     @pytest.mark.parametrize("assay", ASSAYS)
     @pytest.mark.parametrize("organism_h_m", ["human","mouse"])
-    def test_human_mouse_atac_fragment_w_other_h5ad(self, assay, tmpdir, organism_h_m):
+    def test_human_mouse_atac_fragment_w_other_h5ad_unmatched_barcodes(self, assay, tmpdir, organism_h_m):
 
-       # ATAC fragment associated with a dataset with uns.organism_ontology_term_id on approved list but not human or mouse -> fail
+       # human/mouse ATAC fragment w/ other organism h5ads without match barcodes -> fail
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:10090" != organism
         assert "NCBITaxon:9606" != organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
-        print(self.atac.fragment_df)
+        frag_df = pd.read_csv(str(FIXTURES_ROOT / f"valid_{organism_h_m}_fragments.tsv.gz"), sep='\t',header=None)
+        self.atac.fragment_df = frag_df
         self.atac.fragment_file_name = f"valid_{organism_h_m}_fragments.tsv.gz"
         temp_files = to_temp_files(self.atac, tmpdir)
-        #print(temp_files)
         results = process_fragment(**temp_files)
-        #print(results)
         assert results != []
+        assert (
+            "Errors found in Anndata file. Skipping fragment validation."
+        ) in results
+        assert (
+            f"Anndata.obs.organism_ontology_term_id must be one of ['NCBITaxon:9606', 'NCBITaxon:10090']. Got {organism}."
+        ) in results
+
 
 
     @pytest.mark.parametrize("assay",ASSAYS)
@@ -196,21 +185,26 @@ class TestOtherATACValidation:
 
         # human/mouse ATAC fragment w/ other organism h5ads with matching barcodes -> fail
 
-        organism = self.validator.adata.uns["organism_ontology_term_id"]
+        organism = self.atac.adata.uns["organism_ontology_term_id"]
         assert "NCBITaxon:10090" != organism
         assert "NCBITaxon:9606" != organism
         self.atac.adata.obs["assay_ontology_term_id"] = assay
         self.atac.adata.obs["suspension_type"] = "nucleus"
-        self.atac.adata.obs["suspension_type"] = self.validator.adata.obs["suspension_type"].astype("category")
+        self.atac.adata.obs["suspension_type"] = self.atac.adata.obs["suspension_type"].astype("category")
         self.atac.adata.obs["is_primary_data"] = True
-        print(self.atac.fragment_df)
-
+        frag_df = pd.read_csv(str(FIXTURES_ROOT / f"valid_{organism_h_m}_fragments.tsv.gz"), sep='\t',header=None)
+        self.atac.fragment_df = frag_df
         self.atac.adata.obs.reset_index(inplace=True)
         self.atac.fragment_df[3] = self.atac.adata.obs["obs_index"].sample(n=len(self.atac.fragment_df),replace=True).values
-
+        self.atac.adata.obs.set_index("obs_index",inplace=True)
+        assert self.atac.fragment_df[3][0] in self.atac.adata.obs.index
         self.atac.fragment_file_name = f"valid_{organism_h_m}_fragments.tsv.gz"
         temp_files = to_temp_files(self.atac, tmpdir)
-        #print(temp_files)
         results = process_fragment(**temp_files)
-        #print(results)
         assert results != []
+        assert (
+            "Errors found in Anndata file. Skipping fragment validation."
+        ) in results
+        assert (
+            f"Anndata.obs.organism_ontology_term_id must be one of ['NCBITaxon:9606', 'NCBITaxon:10090']. Got {organism}."
+        ) in results
