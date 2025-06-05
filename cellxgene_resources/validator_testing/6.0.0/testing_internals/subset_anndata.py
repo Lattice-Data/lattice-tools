7b28f8dd46bcee9a011d47777787719a5e15b092
corismall <corinn.small@gmail.com>
2025-06-05 14:49:19 -0700

    added in code for visium

--- cellxgene_resources/validator_testing/6.0.0/testing_internals/subset_anndata.py
+++ cellxgene_resources/validator_testing/6.0.0/testing_internals/subset_anndata.py
@@ -34,7 +34,6 @@ def subset_adata(fixture_file:str):
     '''
 
     adata = read_h5ad(FIXTURES_ROOT / fixture_file)
-    new_adata = adata[:5 , :5].copy()  # subsets all attributes, except adata.raw
     raw_matrix = np.array([
         [1, 0, 0, 0, 1],
         [1, 2, 0, 0, 0],
@@ -42,6 +41,7 @@ def subset_adata(fixture_file:str):
         [1, 0, 0, 4, 0],
         [1, 0, 0, 0, 5],
     ],dtype=np.float32)
+
     norm_matrix = np.array([
         [1.1, 0, 0, 0, 1.1],
         [1.1, 2.2, 0, 0, 0],
@@ -50,18 +50,34 @@ def subset_adata(fixture_file:str):
         [1.1, 0, 0, 0, 5.5]
     ],dtype=np.float32)
 
-    if new_adata.raw:
-        raw_var = new_adata.raw.var
+    def _expand_to_visium(start_matrix: np.ndarray, rows: int) -> np.ndarray:
+        return np.concatenate([start_matrix + np.random.randint(0, 100, size=start_matrix.shape) for _ in range(int(np.ceil(rows / start_matrix.shape[0])))])[:rows, ].astype(np.float32)
+
+    if adata.obs["assay_ontology_term_id"].unique()[0] in ["EFO:0022857","EFO:0022858","EFO:0022859"]:
+        raw_matrix = _expand_to_visium(raw_matrix,4992)
+        norm_matrix = _expand_to_visium(norm_matrix,4992)
+        new_adata = adata[:4992 , :5].copy()
+
+    elif adata.obs["assay_ontology_term_id"].unique()[0] == "EFO:0022860":
+        raw_matrix = _expand_to_visium(raw_matrix,14336)
+        norm_matrix = _expand_to_visium(norm_matrix,14336)
+        new_adata = adata[:14336 , :5].copy()
 
     else:
-        raw_var = new_adata.var
+        new_adata = adata[:5 , :5].copy()
+
+    if new_adata.raw:
+        raw_var = new_adata.raw.var[:5]
+
+    else:
+        raw_var = new_adata.var[:5]
         raw_var.drop(columns=["feature_is_filtered"],inplace=True)
 
     new_adata.X = norm_matrix
     new_raw_adata = ad.AnnData(
         X = raw_matrix,
-        obs = new_adata.obs[:5],
-        var =  raw_var[:5]
+        obs = new_adata.obs,
+        var =  raw_var
     )
     new_adata.raw = new_raw_adata
     new_adata.var['feature_is_filtered'] = False
