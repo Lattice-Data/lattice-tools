a245742ed0d851d0b92c264451dc28df0a7506b2
corismall <corinn.small@gmail.com>
2025-06-09 14:48:07 -0700

    update status

--- cellxgene_resources/validator_testing/6.0.0/6-0_cxg661_schema_reference_test.py
+++ cellxgene_resources/validator_testing/6.0.0/6-0_cxg661_schema_reference_test.py
@@ -6,11 +6,8 @@ Check write_labels (add-labels)
 - check uns properties
 
 Should pass:
-(N) schema_reference == "https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/6.0.0/schema.md" in uns
-(N) schema_version == "6.0.0" in uns
-
--> currently both are at 5.3.0
-
+(Y) schema_reference == "https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/6.0.0/schema.md" in uns
+(Y) schema_version == "6.0.0" in uns
 """
 
 
@@ -30,8 +27,8 @@ from fixtures.valid_adatas import (
 @pytest.mark.parametrize("property", ["organism","schema_version","schema_reference"])
 def test_add_labels_uns(validator_with_adatas,property):
 
-    # add_labels check: uns.schema_reference should be modified to 6.0.0 -> pass  # FAILED
-    # add_labels check: uns.schema_version should be modified to 6.0.0 -> pass  # FAILED
+    # add_labels check: uns.schema_reference should be modified to 6.0.0 -> pass
+    # add_labels check: uns.schema_version should be modified to 6.0.0 -> pass
 
     TEMP_DIR = tempfile.mkdtemp()
     schema_version = "6.0.0"
