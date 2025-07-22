b66adb5b7380721bb7f0664a6fdc095ccd7f7959
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2025-07-21 23:03:36 -0700

    print actual filtered files and not raw files

--- scripts/concatenator.py
+++ scripts/concatenator.py
@@ -436,7 +436,7 @@ if __name__ == "__main__":
         else:
             print("Rerun concatenator to generate all filtered fragment files")
             print("Missing following files:")
-            print([meta.download_file_name for meta in fragment_meta if not meta.is_filtered_file_local])
+            print([meta.filtered_fragment_path_name.name for meta in fragment_meta if not meta.is_filtered_file_local])
             sys.exit()
 
     results = run_processing_pool(worker_function, fragment_meta)
