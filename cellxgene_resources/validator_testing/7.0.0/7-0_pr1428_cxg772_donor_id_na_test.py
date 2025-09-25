7dbef0f7624486a314b65d0782dd421142ebef11
corismall <corinn.small@gmail.com>
2025-09-25 12:36:58 -0700

    finished tests for review

--- cellxgene_resources/validator_testing/7.0.0/7-0_pr1428_cxg772_donor_id_na_test.py
+++ cellxgene_resources/validator_testing/7.0.0/7-0_pr1428_cxg772_donor_id_na_test.py
@@ -4,13 +4,15 @@ PR for this issue: https://github.com/chanzuckerberg/single-cell-curation/pull/1
 Testing conditions:
 
 Should pass
+(Y) - tissue_type != "cell line" with normal donor_ids
 (Y) - tissue_type == "cell line" with na for donor_id
 
+
 Should not pass
-(Y) - tissue_type == "cell line" with normal donor_id
-(N) - tissue_type != "cell line" with na for donor_id
-() - tissue_type == "cell line" and donor_id mix of na and normal id
-() - all tissue_types with one random 'na' mixed in with normal ids
+(Y) - tissue_type == "cell line" with all normal donor_ids
+(Y) - tissue_type == "cell line" with donor_id mix of one random 'na' and normal ids
+(N) - tissue_type != "cell line" with all na for donor_ids
+(N) - tissue_type != "cell line"  with donor_id mix of one random 'na' and normal ids
 """
 
 import numpy as np
@@ -49,7 +51,23 @@ class TestDonorIDValidation:
         self.validator = validator_with_adatas
 
 
-    def test_tissue_cell_line_donor_na_passes(self):
+    @pytest.mark.parametrize("tissue_type", NON_CELL_LINE_TISSUES)
+    def test_non_cell_line_valid(self, tissue_type):
+
+        # tissue_type != "cell line" with normal donor_ids
+
+        self.validator.adata.obs["tissue_type"] = tissue_type
+        self.validator.adata.obs["tissue_type"] = self.validator.adata.obs["tissue_type"].astype("category")
+
+        if tissue_type == "primary cell culture":
+            self.validator.adata.obs["tissue_ontology_term_id"] = "CL:0000617"
+
+        self.validator.validate_adata()
+        assert self.validator.is_valid
+        assert self.validator.errors == []
+
+
+    def test_tissue_cell_line_donor_na_valid(self):
 
         # tissue_type == "cell line" with na for donor_id
 
@@ -80,8 +98,7 @@ class TestDonorIDValidation:
     @pytest.mark.parametrize("tissue_type", NON_CELL_LINE_TISSUES)
     def test_tissue_not_cell_line_id_na_invalid(self, tissue_type):
 
-        # tissue_type != "cell line" with donor_id == "na" -> currently allowed, shouldn't be
-
+        # tissue_type != "cell line" with donor_id == "na" -> currently passing, shouldn't be allowed
 
         self.validator.adata.obs["tissue_type"] = tissue_type
         self.validator.adata.obs["tissue_type"] = self.validator.adata.obs["tissue_type"].astype("category")
@@ -92,64 +109,45 @@ class TestDonorIDValidation:
 
         self.validator.validate_adata()
         assert not self.validator.is_valid
-        '''assert (
-
-        ) in self.validator.errors'''
+        '''assert () in self.validator.errors'''
 
 
     def test_tissue_cell_line_with_normal_ids_and_na_invalid(self):
 
-            # tissue_type == "cell line" and donor_id mix of na and normal
+        # tissue_type == "cell line" and donor_id with one random na + normal ids
 
-            original_donor_ids = self.validator.adata.obs["donor_id"]
-            self.validator.adata = make_valid_cell_line_fixture(self.validator.adata)
-            self.validator.adata.obs["donor_id"] = original_donor_ids
-            random_index = np.random.randint(0, (self.validator.adata.obs.shape[0] - 1))
-            self.validator.adata.obs.loc[self.validator.adata.obs.index[random_index], "donor_id"] = "na"
-            self.validator.validate_adata()
-            assert not self.validator.is_valid
-            # trying assert for common error string ending instead of matching on full error
-            # string for each dev term in test fixture
-            for error in self.validator.errors:
-                assert error.endswith("Values must be one of ['na'] when 'tissue_type' is 'cell line'.")
-
-
-    @pytest.mark.parametrize("tissue_type", ALL_TISSUE_TYPES)
-    def test_tissue_not_cell_line_donor_id_one_na_invalid(self, tissue_type):
-
-        # all tissue_types with one random 'na' mixed in with normal ids
-
-        self.validator.adata.obs["tissue_type"] = tissue_type
-        if tissue_type == "cell line":
-            self.validator.adata.obs['tissue_ontology_term_id'] = 'CVCL_2830'
-
-        # errors for each unique value in ontologies, generate list of tuples
-        # for error string matching
-        ontology_values = []
-        for ontology in NA_COLUMNS:
-            for value in self.validator.adata.obs[ontology].unique():
-                ontology_values.append((ontology, value))
-
-        self.validator.adata.obs["tissue_type"] = self.validator.adata.obs["tissue_type"].astype("category")
-        # single "unknown should be at index 0, start at 1 to avoid replacing one instance"
-        random_index = np.random.randint(1, (self.validator.adata.obs.shape[0] - 1))
-        for col in NA_COLUMNS:
-            self.validator.adata.obs[col] = self.validator.adata.obs[col].cat.add_categories("na")
-            self.validator.adata.obs.loc[self.validator.adata.obs.index[random_index], col] = "na"
-
+        original_donor_ids = self.validator.adata.obs["donor_id"]
+        self.validator.adata = make_valid_cell_line_fixture(self.validator.adata)
+        self.validator.adata.obs["donor_id"] = original_donor_ids
+        random_index = np.random.randint(0, (self.validator.adata.obs.shape[0] - 1))
+        self.validator.adata.obs["donor_id"] = self.validator.adata.obs["donor_id"].cat.add_categories(["na"])
+        self.validator.adata.obs.loc[self.validator.adata.obs.index[random_index], "donor_id"] = "na"
         self.validator.validate_adata()
         assert not self.validator.is_valid
+        # trying assert for common error string ending instead of matching on full error
+        # string for each dev term in test fixture
+        for error in self.validator.errors:
+            assert error.endswith("Values must be one of ['na'] when 'tissue_type' is 'cell line'.")
 
-        # this works, but probably should be 2 tests to have less complicated logic for asserts
-        if tissue_type != "cell line":
-            assert (
-                "ERROR: 'na' in 'development_stage_ontology_term_id' is not allowed. "
-                "When 'tissue_type' is not 'cell line', 'development_stage_ontology_term_id' cannot be 'na'."
-            ) in self.validator.errors
-        else:
-            for ontology, value in ontology_values:
-                error_string = (
-                    f"ERROR: '{value}' in '{ontology}' is not a valid value of '{ontology}'. "
-                    f"When 'tissue_type' is 'cell line', '{ontology}' MUST be 'na'."
-                )
-                assert error_string in self.validator.errors
+
+    @pytest.mark.parametrize("tissue_type", NON_CELL_LINE_TISSUES)
+    def test_tissue_not_cellline_donor_id_one_na_invalid(self, tissue_type):
+
+        # tissue_type != cell line and donor_id with one random 'na' + normal ids -> currently passing, shouldn't be allowed
+
+        self.validator.adata.obs["tissue_type"] = tissue_type
+        self.validator.adata.obs["tissue_type"] = self.validator.adata.obs["tissue_type"].astype("category")
+
+        if tissue_type == "primary cell culture":
+            self.validator.adata.obs["tissue_ontology_term_id"] = "CL:0000617"
+
+        random_index = np.random.randint(0, (self.validator.adata.obs.shape[0] - 1))
+        self.validator.adata.obs["donor_id"] = self.validator.adata.obs["donor_id"].cat.add_categories(["na"])
+        self.validator.adata.obs.loc[self.validator.adata.obs.index[random_index], "donor_id"] = "na"
+        self.validator.validate_adata()
+        assert not self.validator.is_valid
+        '''assert () in self.validator.errors'''
+
+
+
+
