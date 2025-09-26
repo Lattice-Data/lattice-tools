236884cae2585e2802dce5fa3a3955d46b6205b9
Jennifer Chien <52470876+jychien@users.noreply.github.com>
2025-09-26 12:15:35 -0700

    Jason bcp (#264)
    * initial curated mx ntbk, expected outs files
    
    * comment on obs.lab
    
    * add multiome outputs
    
    * remove serialization, update to current file structure
    
    * update to current template
    
    * filter out qc metrics
    
    * tidy up commnets, remove duplicate library col
    
    * add scanpy metrics calc
    
    * Adding ontologizing of standard cellxgene columns
    
    * add guide metadata in adata.obs and adata.uns and library_name to adata.obs
    
    * if disease is unknown, have term_id be PATO:0000461
    
    * update metrics from CellRanger
    
    * update reads mapped metrics
    
    * keep Cells category, add per library metrics
    
    * fix target_genomic_regions formating, simplify library_name, add title, and convert boolean columns
    
    * update to genetic_perturbation_id and clean var variables
    
    * Adding sample_probe_barcode to keep in gather_metrics. Adding get_gid() function to convert tab name to gid and added calls at appropriate locations
    
    * Add functions for processing exact matches and selecting best protospacer matches in curated_matrix.ipynb. Introduced CSV handling and subprocess execution for GuideScan integration.
    
    * moved curated matrix notebook into python script and added parallelization script
    
    * update logging, update to "na", added "order" to output filename, add block to tar file opening
    
    * format num_umis_guide_id and drop "\" columns
    
    * need to strip newline at end of GroupID
    
    * Delete bcp/curated_matrix.ipynb
    
    No longer need since we've moved to python script
    
    * Delete bcp/expected_files.ipynb
    
    No longer up to date with current directory structure on S3
    
    * Changing delimiter for target_genomic_regions from : to -, removing target_genomic_regions if NaN, adding better handling of self_reported_ethncitiy mapping
    
    * Quick fix for when standard column is all n/a so dropped before ontologizing
    
    * Fixing issue when self_reported_ethnicity was all na being mishandled in map_ontologies(), switched drop column indicater from \\ to !
    
    * Adjustments now that only na will be used in sheet not n/a.
    
    ---------
    
    Co-authored-by: jahilton <jahilton@stanford.edu>
    Co-authored-by: Jchaffer787 <77802611+Jchaffer787@users.noreply.github.com>
    Co-authored-by: Idan <gabdank@stanford.edu>

---
+++ bcp/parallel_curated.py
@@ -0,0 +1,158 @@
+import argparse
+import multiprocessing
+import traceback
+import sys
+import subprocess
+import os
+import re
+
+
+EPILOG = f"""
+Script run parallel_curated.py in a parallel fashion
+
+Example:
+    python parallel_curated.py --bucket czi-psomagen --sheet your_sheet_id --project marson-mapping-grns-perturb-seq --groupfile groups.txt
+    python parallel_curated.py --bucket czi-psomagen --sheet your_sheet_id --project marson-mapping-grns-perturb-seq --grouplist CD4i_R1L01 CD4i_R1L02
+
+For more details:
+    python %(prog)s --help
+
+"""
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description=__doc__, 
+        epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "--bucket", 
+        "-b",
+        help="bucket where h5 files live",
+        default="czi-psomagen"
+    )
+    parser.add_argument(
+        "--sheet", 
+        "-s",
+        help="google sheet id of metadata"
+    )
+    parser.add_argument(
+        "--project", 
+        "-p",
+        help="Name of the project directory on S3 bucket"
+    )
+    parser.add_argument(
+        "--groupfile",
+        "-f",
+        help="the file containing GroupID of the sample that for curated matrices generation"
+    )
+    parser.add_argument(
+        "--grouplist",
+        "-l",
+        help="a commandline list containing GroupID of the sample that for curated matrices generation",
+        nargs="+",
+        default=list()
+    )
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
+    	parser.print_help()
+    	sys.exit()
+    return args
+
+
+def create_arguments(args, groups):
+    '''
+    Creates list of tuples that are the commandlines for curate_matrices.py
+
+    return: list of tuple commandlines
+    '''
+    commands_list = []
+    for i in range(len(groups)):
+        new_tuple = ("--bucket", args.bucket, "--sheet", args.sheet, "--project", args.project, "--group", groups[i])
+        commands_list.append(new_tuple)
+    return commands_list
+
+
+def make_groups_list(args):
+    '''
+    Allows comments in the groupfile, which will be skipped
+
+    return: list of GroupIDs
+    '''
+    if args.grouplist:
+        return args.grouplist
+    else:
+        with open(args.groupfile, "r") as f:
+            return [line.strip() for line in f if not line.startswith("#")]
+
+
+# Define the script you want to run in parallel
+CURATE_SCRIPT = "curate_matrices.py"
+
+def run_curate(commands_tuple):
+    command = [sys.executable, CURATE_SCRIPT] + list(commands_tuple)
+    print(f"Starting command: {' '.join(command)}")
+
+    try:
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            check=True,
+            env={**os.environ, 'PYTHONUNBUFFERED': '1'}
+        )
+        return f"SUCCESS: Command `{' '.join(command)}` finished.\n{result.stdout.strip()}"
+    except subprocess.CalledProcessError as e:
+        return f"ERROR: Command `{' '.join(command)}` failed with exit code {e.returncode}.\n{e.stderr.strip()}"
+    except Exception as e:
+        return f"ERROR: An unexpected error occurred for command `{' '.join(command)}`: {e}"
+
+
+
+args = getArgs()
+
+
+if __name__ == "__main__":
+    results = list()
+    groups = []
+    groups = make_groups_list(args)
+    commands_list = create_arguments(args, groups)
+    workers = min(len(groups), 50)
+    with multiprocessing.Pool(processes=workers) as pool:
+        iterator = pool.imap(run_curate, commands_list)
+        for curate_log in iterator:
+            try:
+                results.append(curate_log)
+            except StopIteration:
+                break
+            except Exception as e:
+                print(f"ERROR: {e}")
+                results.append((curate_log, e))
+
+    print("FINAL RESULTS:")
+    print("=" * 80)
+    print("SUCCESS:")
+    log_results = {}
+    log_results['success'] = []
+    log_results['error'] = {}
+    for r in results:
+        match = re.search(r"--group\s(.*?)\`", r)
+        groupid = match.group(1)
+        if r.startswith('SUCCESS'):
+            log_results['success'].append(groupid)
+        elif r.startswith('ERROR'):
+            log_results['error'][groupid] = r
+
+    print(f'Total successful GroupID:\t{len([i for i in results if i.startswith("SUCCESS")])}')
+    print(f'Successful GroupIDs:\t{log_results["success"]}')
+    if log_results['error']:
+        print("FAILURE:")
+        print(f'Total error GroupID:\t{len(log_results["error"].keys())}')
+        for groupid, error in log_results['error'].items():
+            print(f'Error GroupID:\t{groupid}')
+            print(f'Error logging:\t{error}')
+    else:
+        print("NO ERRORS")
+
+    
