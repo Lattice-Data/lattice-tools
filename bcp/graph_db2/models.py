b0bf2d0fd266030caddb44074f4c86feb86443fb
Brian Mott <125435496+brian-mott@users.noreply.github.com>
2026-08-26 09:43:54 -0700

    reorganize to avoid circular import

--- bcp/graph_db2/models.py
+++ bcp/graph_db2/models.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import ClassVar, TypeAlias
+from typing import ClassVar, Collection, TypeAlias
 from urllib.parse import urljoin
 
 import requests
 from connection import get_connection
 from constants import ABSTRACT_MAPPING, DEFAULT_MODE, EXCLUDED_SCHEMAS
 from db2_flattener.gather.lattice import Connection
+from db2_flattener.gather.gatherer import DB2Gatherer
 from db2_flattener.schema.generate import SchemaIDs
 
 
@@ -129,3 +131,60 @@ class LatticeNode:
 GroupedBatchRequest: TypeAlias = dict[str, set]
 BatchResponse: TypeAlias = list[dict]
 GraphDict = dict[str, LatticeNode]
+
+
+def group_batch_request(uuid_paths: Collection) -> GroupedBatchRequest:
+    """
+    DB2Gatherer needs API name and UUID to make batch request
+    Returns dict with key of API name and set of UUIDs to request
+    """
+    grouped_paths = defaultdict(set)
+    for path in uuid_paths:
+        # skip if already in cache
+        if path in LatticeNode._cache:
+            continue
+        node = LatticeNode(path)
+        grouped_paths[node.schema_ids.api_name].add(node.uuid)
+
+    return grouped_paths
+
+
+def batch_request_chunk_and_fetch(
+    grouped_request: GroupedBatchRequest, gatherer: DB2Gatherer
+) -> BatchResponse:
+    """
+    After grouping many objects by type, send off a batch request per type via DB2Gatherer.chunk_and_fetch()
+    """
+    responses = []
+    for api_name, uuid_set in grouped_request.items():
+        responses.extend(
+            gatherer.chunk_and_fetch(
+                obj_type=api_name,
+                object_ids=uuid_set,
+            )
+        )
+    return responses
+
+
+def fetch_all_references_from_cache(node: LatticeNode, gatherer: DB2Gatherer):
+    """
+    After one API request stores a JSON response in the LatticeNode cache, can
+    then run this function to batch request all references not excluded.
+    Should call this prior to constructing adjacency dict
+    """
+    if not LatticeNode._cache:
+        print("No items in cache, exiting")
+        return
+
+    running = True
+
+    while running:
+        cache_ids = set()
+        for json_object in LatticeNode._cache.values():
+            cache_ids.update(LatticeNode.get_ids(json_object, node.excluded))
+        grouped_request = group_batch_request(cache_ids)
+        if not grouped_request:
+            running = False
+            break
+        responses = batch_request_chunk_and_fetch(grouped_request, gatherer)
+        LatticeNode.batch_cache_update(responses)
--- bcp/graph_db2/schema.py
+++ bcp/graph_db2/schema.py
@@ -1,5 +1,10 @@
-from functools import cache
+from dataclasses import dataclass
+from functools import cache, cached_property
+from typing import Protocol
 
+from db2_flattener.gather.gatherer import DB2Gatherer
+from db2_flattener.gather.lattice import Connection
+from db2_flattener.schema.constants import Configs
 from db2_flattener.schema.generate import YAML_PATH, load_yaml_config
 
 
